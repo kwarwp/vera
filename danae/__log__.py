c3e2fbe5c50f6ca8d93f4f95b27a7d39cd5b74a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-06 07:20:50 -0300

    Created danae/__log__.py at Timestamp: 2018-09-06 10:20:49

---
+++ danae/__log__.py
@@ -0,0 +1,25 @@
+
+{'date': 'Thu Sep 06 2018 07:16:11.614 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 406
+    Jogo().inicia()
+  module <module> line 399
+    self.mesa = Mesa(jogadores)
+  module <module> line 329
+    self.jogadores_ativos = self.jogadores = [Jogador(jogador, self) for jogador in jogadores]
+  module <module> line 274
+    self.jogador = self.inicia(jogador)
+  module <module> line 279
+    except TypeError:
+NameError: name 'GET_JOGADOR' is not defined
+'''},
