24ae9a330d7b913fe8d2830f23d682292ee3cb7d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-06 08:26:03 -0300

    Created libby/main.py at Timestamp: 2018-09-06 11:26:03

---
+++ libby/main.py
@@ -0,0 +1,15 @@
+# vera.libby.main.py
+from random import shuffle
+
+
+class Jogador:
+    def __init__(self):
+        self.chance = list(range(20))
+
+    def joga(self, mesa):
+        desiste = self.chance.pop() < 2 if self.chance else True
+        return desiste
+
+
+def libby():
+    return Jogador()
