24f7f26d2b05cd8188cf9e5ebcba78b3ed5d8933
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 11:29:27 -0300

    Saved grace/__log__.py at Timestamp: 2018-09-12 14:29:26

--- grace/__log__.py
+++ grace/__log__.py
@@ -51,3 +51,21 @@ ImportError: cannot import name 'main'
     STYLE["width"] = 800
 AttributeError: 'module' object has no attribute '__setitem__'
 '''},
+{'date': 'Wed Sep 12 2018 11:25:29.630 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    main()
+  module _spy.tesouro.main line 215
+    return Jogo(gui=gui, jogadores=[JogadorSimples(None, jogador) for jogador in jogadores])
+TypeError: 'module' object is not callable
+'''},
