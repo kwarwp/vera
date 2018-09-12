288fcc13d7363728ca21ef50d372504a42e84cfd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 11:42:48 -0300

    Saved grace/__log__.py at Timestamp: 2018-09-12 14:42:48

--- grace/__log__.py
+++ grace/__log__.py
@@ -81,3 +81,19 @@ Exception: unexpected token: '.'
   module '$exec_1854' line 4
 main(gui=Gui())
 '''},
+{'date': 'Wed Sep 12 2018 11:38:51.212 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    def main(jogadores=JOGADORES, gui=GUI):
+NameError: name 'JOGADORES' is not defined
+'''},
