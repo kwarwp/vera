2fe1aaf8c04cc2cadcdcddd055dcc8d255c91819
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 11:42:33 -0300

    Saved grace/main.py at Timestamp: 2018-09-12 14:42:33

--- grace/main.py
+++ grace/main.py
@@ -1,3 +1,28 @@
 # vera.grace.main.py
-from _spy.tesouro.main import main
-main()
+from _spy.tesouro.main import  Gui
+from _spy.tesouro.tesouro import  Jogo
+
+
+def main(jogadores=JOGADORES, gui=GUI):
+    class JogadorSimples:
+        def __init__(self, joga, nome):
+            _chance = list(range(20))
+            shuffle(_chance)
+            self.joga, self.jogadas, self.nome = joga, _chance, nome
+            self._inicia(nome)
+
+        def _inicia(self, jogador):
+            get_joga = "from {mod}.main import {mod}; self.joga = {mod}()"
+            try:
+                exec(get_joga.format(mod=jogador))
+                self.nome = self.nome.upper()
+            except (ImportError, TypeError):
+                self.joga = self._joga
+
+        def _joga(self, _):
+            return self.jogadas.pop() < 2
+
+    return Jogo(gui=gui, jogadores=[JogadorSimples(None, jogador) for jogador in jogadores])
+
+#print(main, Gui)
+main(gui=Gui()).inicia()
