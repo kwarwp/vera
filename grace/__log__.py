76432734447726fa42026382e5451600bbb8000f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 11:53:14 -0300

    Saved grace/__log__.py at Timestamp: 2018-09-12 14:53:13

--- grace/__log__.py
+++ grace/__log__.py
@@ -117,3 +117,21 @@ NameError: name 'JOGADORES' is not defined
     shuffle(_chance)
 NameError: name 'shuffle' is not defined
 '''},
+{'date': 'Wed Sep 12 2018 11:49:16.807 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    main(gui=Gui()).inicia()
+  module <module> line 26
+    gui.inicia()
+AttributeError: 'Gui' object has no attribute 'inicia'
+'''},
