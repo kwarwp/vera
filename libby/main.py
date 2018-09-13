f04f7ba48d60e514627694c917149b8d479dbde9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-13 04:43:34 -0300

    Saved libby/main.py at Timestamp: 2018-09-13 07:43:34

--- libby/main.py
+++ libby/main.py
@@ -4,7 +4,8 @@ from random import shuffle
 
 class Jogador:
     def __init__(self):
-        self.chance = list(range(20))
+        self.chance = list(range(30))
+        shuffle(self.chance)
 
     def joga(self, mesa):
         desiste = self.chance.pop() < 2 if self.chance else True
