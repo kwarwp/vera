d0f0168cd690ae0fc29e5d8cdf3b659bc3546eec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-13 08:15:55 -0300

    Created sara/main.py at Timestamp: 2018-09-13 11:15:55

---
+++ sara/main.py
@@ -0,0 +1,27 @@
+# vera.sara.main.py
+from random import shuffle
+"0:roxanne 1:libby 2:sara 3:kellee 4:courtney 5:angie 6:naomi 7:tracy 8:morgan"
+
+class Jogador:
+    def __init__(self):
+        self.chance = list(range(20))
+        self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
+        self.maior_tesouro, self.maior_joias, = [0]*6
+        self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
+        self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = [[]]*6
+
+    def joga(self, mesa):
+        self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
+        self.maior_tesouro, self.maior_joias,\
+        self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
+        self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = mesa.atualiza()
+        medo = self.perigos > 4
+        cobica = self.artefatos > 2
+        cautela = self.cartas > 9
+        ambicao = self.maior_tesouro > self.tesouros_jogadores[0]        
+        sorte = self.chance.pop() < 2 if self.chance else True
+        return medo or cobica or cautela or ambicao or sorte
+
+
+def sara():
+    return Jogador()
