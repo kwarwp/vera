c633f1f19287445932682f6b806569df7ac0220b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-13 08:41:04 -0300

    Saved roxanne/main.py at Timestamp: 2018-09-13 11:41:04

--- roxanne/main.py
+++ roxanne/main.py
@@ -4,7 +4,7 @@ from random import shuffle
 
 class Jogador:
     def __init__(self):
-        self.chance = list(range(20))
+        self.chance = list(range(30))
         self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
         self.maior_tesouro, self.maior_joias, = [0]*6
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
@@ -15,9 +15,9 @@ class Jogador:
         self.maior_tesouro, self.maior_joias,\
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
         self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = mesa.atualiza()
-        medo = self.perigos > 4
+        medo = self.perigos > 2
         cobica = self.artefatos > 2
-        cautela = self.cartas > 9
+        cautela = self.cartas > 6
         ambicao = self.maior_tesouro > self.tesouros_jogadores[0]        
         sorte = self.chance.pop() < 2 if self.chance else True
         return medo or cobica or cautela or ambicao or sorte
