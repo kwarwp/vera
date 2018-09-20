0de1a38b283a3dbf3a10a6fdc035d8cc7f82dee4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-20 09:36:48 -0300

    Saved courtney/main.py at Timestamp: 2018-09-20 12:36:47

--- courtney/main.py
+++ courtney/main.py
@@ -1,11 +1,10 @@
-# vera.courtney.main.py
+# vera.courtney.main.py 
 from random import shuffle
 "0:roxanne 1:libby 2:sara 3:kellee 4:courtney 5:angie 6:naomi 7:tracy 8:morgan"
 
 class Jogador:
     def __init__(self):
         self.chance = list(range(30))
-        shuffle(self.chance)
         self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
         self.maior_tesouro, self.maior_joias, = [0]*6
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
@@ -25,5 +24,5 @@ class Jogador:
         return sorte
 
 
-def courtney():
-    return Jogador()
+def roxanne():
+    return Jogado278
