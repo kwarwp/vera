ef0ca1a94d448b3987af76f8f27a214cd8b65de0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-20 09:36:45 -0300

    Saved roxanne/main.py at Timestamp: 2018-09-20 12:36:44

--- roxanne/main.py
+++ roxanne/main.py
@@ -5,6 +5,7 @@ from random import shuffle
 class Jogador:
     def __init__(self):
         self.chance = list(range(30))
+        shuffle(self.chance) 
         self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
         self.maior_tesouro, self.maior_joias, = [0]*6
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
@@ -25,4 +26,4 @@ class Jogador:
 
 
 def roxanne():
-    return Jogado278
+    return Jogador()
