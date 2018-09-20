053af0df4174c01abf3633d94120171e0d7cc345
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-20 09:54:46 -0300

    Saved grace/__log__.py at Timestamp: 2018-09-20 12:54:46

--- grace/__log__.py
+++ grace/__log__.py
@@ -876,3 +876,10 @@ SyntaxError: invalid syntax
     return Jogado278
 NameError: name 'Jogado278' is not defined
 '''},
+{'date': 'Thu Sep 20 2018 09:54:50.552 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module angie.main line 10
+  self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\ 
+                                                                   ^
+SyntaxError: invalid syntax
+'''},
