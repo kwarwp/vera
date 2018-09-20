f4a7ab27e760166d42b1cb7f46c5ef071db0f3ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-20 10:00:42 -0300

    Saved angie/main.py at Timestamp: 2018-09-20 13:00:42

--- angie/main.py
+++ angie/main.py
@@ -8,7 +8,7 @@ class Jogador:
         self.chance = list(range(30))
         shuffle(self.chance)
         self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\ 
-        self.maior_tesouro, self.maior_joias, = [0]*6
+        self.maior_tesouro, self.maior_joias, = [5]*6
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
         self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = [[]]*6
 
