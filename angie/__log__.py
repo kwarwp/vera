ae58fd8d962ea89cef74bfcbd8083e16626f1859
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-20 10:10:08 -0300

    Created angie/__log__.py at Timestamp: 2018-09-20 13:10:08

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Sep 20 2018 10:09:40.946 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\ 
+                                                                   ^
+SyntaxError: invalid syntax
+'''},
