2cfcc719b885054b5fa4d7397ae5fa2949d34ff7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 09:49:58 -0300

    Saved grace/__log__.py at Timestamp: 2018-09-27 12:49:58

--- grace/__log__.py
+++ grace/__log__.py
@@ -1761,3 +1761,31 @@ ValueError: need more than 5 values to unpack
     self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
 ValueError: need more than 5 values to unpack
 '''},
+{'date': 'Thu Sep 27 2018 09:50:24.908 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    main(gui=Gui()).inicia()
+  module <module> line 27
+    return Jogo(gui=gui, jogadores=[JogadorSimples(None, jogador) for jogador in jogadores])
+  module <module> line 13
+    self._inicia(nome)
+  module <module> line 20
+    except (ImportError, TypeError):
+  module <module> line 1
+    from tracy.main import tracy; self.joga = tracy().joga
+  module tracy.main line 29
+    return Jogador()
+  module tracy.main line 9
+    self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
+ValueError: need more than 5 values to unpack
+'''},
