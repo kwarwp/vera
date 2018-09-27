16fcc2eaa4d596b26dd8b6b04e2045337e7c47dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 10:12:00 -0300

    Saved courtney/main.py at Timestamp: 2018-09-27 13:12:00

--- courtney/main.py
+++ courtney/main.py
@@ -16,15 +16,14 @@ class Jogador:
         self.maior_tesouro, self.maior_joias,\
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
         self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = mesa.atualiza()
-        medo = self.perigos > 5
-        analista =  self.jogadores_jogando > 5
-        cobica = self.artefatos > 3
-        cautela = self.cartas > 10
-        ambicao = self.maior_tesouro > self.tesouros_jogadores[6]        
+        medo = self.perigos > 4
+        cobica = self.artefatos > 2
+        cautela = self.cartas > 9
+        ambicao = self.maior_tesouro > self.tesouros_jogadores[0]        
         sorte = self.chance.pop() < 2 if self.chance else True
         #return medo or cobica or cautela or ambicao or sorte
-        return sorte or analista
+        return sorte
 
 
-def courtney():
+def courtney ():
     return Jogador()
