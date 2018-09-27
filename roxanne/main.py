4e2b97df4c02fb370dd5725752f7c884d971a850
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 10:12:33 -0300

    Saved roxanne/main.py at Timestamp: 2018-09-27 13:12:32

--- roxanne/main.py
+++ roxanne/main.py
@@ -19,10 +19,10 @@ class Jogador:
         medo = self.perigos > 4
         cobica = self.artefatos > 2
         cautela = self.cartas > 7
-        ambicao = self.maior_tesouro > self.tesouros_jogadores[3]        
+        ambicao = self.maior_tesouro > self.tesouros_jogadores[0]        
         sorte = self.chance.pop() < 2 if self.chance else True
         #return medo or cobica or cautela or ambicao or sorte
-        return cautela and sorte
+        return sorte
 
 
 def roxanne():
