6ee9382c0c815b9cc4c7f98f9e83d6cda891525d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 09:29:49 -0300

    Saved angie/main.py at Timestamp: 2018-09-27 12:29:48

--- angie/main.py
+++ angie/main.py
@@ -17,7 +17,7 @@ class Jogador:
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
         self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = mesa.atualiza()
         medo = self.perigos > 4
-        cobica = self.artefatos > 2
+        cobica = self.artefatos > 1
         cautela = self.cartas > 10
         ambicao = self.maior_tesouro > self.tesouros_jogadores[20]        
         sorte = self.chance.pop() < 2 if self.chance else True
