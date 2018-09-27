b538a015c4105da6bc96b3c609f177ae9a58a327
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 10:17:19 -0300

    Created naomi/__log__.py at Timestamp: 2018-09-27 13:17:18

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Sep 27 2018 10:17:20.852 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  ambicao = self.maior_tesouro 1 > self.tesouros_jogadores[0]        
+                                ^
+SyntaxError: invalid syntax
+'''},
