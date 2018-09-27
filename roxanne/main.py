7a28b617e4290f97ba8c8a252e0e7ab80afaca2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 08:56:04 -0300

    Saved roxanne/main.py at Timestamp: 2018-09-27 11:56:04

--- roxanne/main.py
+++ roxanne/main.py
@@ -16,7 +16,7 @@ class Jogador:
         self.maior_tesouro, self.maior_joias,\
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
         self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = mesa.atualiza()
-        medo = self.perigos > 6
+        medo = self.perigos > 4
         cobica = self.artefatos > 1
         cautela = self.cartas > 8
         ambicao = self.maior_tesouro > self.tesouros_jogadores[3]        
