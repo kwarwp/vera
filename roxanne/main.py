443c819b5b3cd8e42d7b25beeb7de1764ce1b48b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 08:22:16 -0300

    Saved roxanne/main.py at Timestamp: 2018-09-27 11:22:16

--- roxanne/main.py
+++ roxanne/main.py
@@ -16,9 +16,9 @@ class Jogador:
         self.maior_tesouro, self.maior_joias,\
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
         self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = mesa.atualiza()
-        medo = self.perigos > 3
-        cobica = self.artefatos > 4
-        cautela = self.cartas > 7
+        medo = self.perigos > 4
+        cobica = self.artefatos > 2
+        cautela = self.cartas > 6
         ambicao = self.maior_tesouro > self.tesouros_jogadores[0]        
         sorte = self.chance.pop() < 2 if self.chance else True
         #return medo or cobica or cautela or ambicao or sorte
