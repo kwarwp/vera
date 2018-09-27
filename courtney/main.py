d620efaa7ac5536e0213cceba19a647cf5c9da68
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-27 09:42:25 -0300

    Saved courtney/main.py at Timestamp: 2018-09-27 12:42:25

--- courtney/main.py
+++ courtney/main.py
@@ -8,7 +8,7 @@ class Jogador:
         self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
         self.maior_tesouro, self.maior_joias, = [0]*10
         self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
-        self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = [[]]*10
+        self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = [[]]*6
 
     def joga(self, mesa):
         self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
@@ -18,7 +18,7 @@ class Jogador:
         coragem = self.perigos > 4
         cobica = self.artefatos > 3
         cautela = self.cartas > 10
-        ambicao = self.maior_tesouro > self.tesouros_jogadores[1]        
+        ambicao = self.maior_tesouro > self.tesouros_jogadores[5]        
         sorte = self.chance.pop() < 2 if self.chance else True
         #return coragem or cobica or cautela or ambicao or sorte
         return ambicao or coragem
