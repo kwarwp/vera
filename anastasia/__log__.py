b403cc414b9b16ff0718d426d37ec580396ccd03
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-01 18:12:47 -0300

    Created anastasia/__log__.py at Timestamp: 2018-10-01 21:12:47

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 01 2018 18:11:52.285 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 34
+  inicio():    
+           ^
+SyntaxError: invalid syntax
+'''},
