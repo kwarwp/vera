0c9c740351ec7e1ab079032afe9daf266f5f2d23
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:39:54 -0300

    Created ruzwana/main.py at Timestamp: 2018-10-25 14:39:54

---
+++ ruzwana/main.py
@@ -0,0 +1,34 @@
+# vera.ruzwana.main.py
+"""
+kathryn - Baralho - Joias - Rodada
+callie - Perigo - Perder - Tesouro
+ruzwana - Ficar - Turno - Sair
+natalia - Mesa - Iniciar - Jogador
+lisa - Decisao - Morrer - Partida
+meredith - universo
+"""
+class Turno:
+    def _init_(self):
+        self.perigo = Perigo()
+        
+    def apresente(self):
+        self.perigo.apresente()
+        
+class Ficar:
+    def _init_(self):
+        self.Mesa = Mesa()
+        
+    def apresente(self):
+        self.Mesa.apresente()
+        
+class Sair:
+    def _init_(self):
+        self.Mesa = Mesa()
+        
+    def apresente(self):
+        self.Mesa.apresente()
+        
+if __name__ == "_main_":
+    mesa = mesa()
+    mesa.apresente()
+    
