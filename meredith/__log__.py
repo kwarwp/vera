dbc516b949a7040ea11cabe33900a1915543dc0a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:38:28 -0300

    Created meredith/__log__.py at Timestamp: 2018-10-25 14:38:28

---
+++ meredith/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Thu Oct 25 2018 11:38:25.898 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from lisa.main import Partida
+  module lisa.main line 19
+    if __lisa__ == "__main__":
+NameError: name '__lisa__' is not defined
+'''},
