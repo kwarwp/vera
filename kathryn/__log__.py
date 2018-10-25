9f54b7f95b7fae4a8bce31629f3dc2be48b160d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:52:53 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-10-25 14:52:53

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -6,3 +6,10 @@
                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Oct 25 2018 11:52:53.645 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  self.jogador = Jogador()
+  ^
+IndentationError: expected an indented block
+'''},
