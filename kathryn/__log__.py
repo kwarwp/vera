d478d5778f350acf0e9106571d5da76310908ece
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:50:55 -0300

    Created kathryn/__log__.py at Timestamp: 2018-10-25 14:50:55

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Oct 25 2018 11:50:53.479 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 29
+  if __name__ == "__main__"
+                            ^
+SyntaxError: invalid syntax
+'''},
