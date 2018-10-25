e345bd82bd2259d2e1c947ca30efa27cb156bac8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:57:31 -0300

    Saved lisa/__log__.py at Timestamp: 2018-10-25 14:57:31

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -55,3 +55,38 @@ Traceback (most recent call last):
     self.rodada = Rodada() 
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu Oct 25 2018 11:57:32.713 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from kathryn.main import Rodada 
+  module kathryn.main line 7
+    from kathryn.main import Baralho
+ImportError: cannot import name 'Baralho'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from kathryn.main import Rodada 
+  module kathryn.main line 7
+    from kathryn.main import Baralho
+'''},
