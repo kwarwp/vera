44a6d9ea1321de61fc84e7a0c9ea3e49f8b00adb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:37:33 -0300

    Created lisa/main.py at Timestamp: 2018-10-25 14:37:33

---
+++ lisa/main.py
@@ -0,0 +1,26 @@
+# vera.lisa.main.py
+from kathryn.main import Rodada 
+"""
+kathryn - Baralho - Joias - Rodada
+callie - Perigo - Perder - Tesouro
+ruzwana - Ficar - Turno - Sair
+natalia - Mesa - Iniciar - Jogador
+lisa - Decisão - Morrer - Partida
+meredith - Universo
+"""
+
+
+class Partida:
+    def __init__(self):
+        self.rodada = Rodada() 
+        
+    def inicia(self):     
+        self.rodada.comeca()
+if __lisa__ == "__main__":
+    partida = Partida()
+    partida.inicia()
+    
+        
+        
+        
+        
