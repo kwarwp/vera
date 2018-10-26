5d0b2955e9dbcda1d02c5a30bc9dbab59112b7a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 21:25:46 -0300

    Created natalia/__log__.py at Timestamp: 2018-10-26 00:25:46

---
+++ natalia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Oct 25 2018 21:25:44.250 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 34
+  del __init__(self):
+                     ^
+SyntaxError: invalid syntax
+'''},
