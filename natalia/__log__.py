4a8942241239c7c9e2795acfa5965eaab44f3ad0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 21:26:36 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-26 00:26:36

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -6,3 +6,10 @@
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Oct 25 2018 21:26:35.607 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 36
+  del retorna():
+                ^
+SyntaxError: invalid syntax
+'''},
