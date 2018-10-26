313eb495d42fe3b24eb2604fb5e3c989af6c125f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 21:36:46 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-26 00:36:45

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -233,3 +233,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Oct 25 2018 21:36:45.223 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  class Jogador:
+  ^
+IndentationError: expected an indented block
+'''},
