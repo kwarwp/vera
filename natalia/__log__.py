9d0b26155c84311470528bee04c603d2ee302751
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 21:29:48 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-26 00:29:48

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -84,3 +84,19 @@ NameError: name 'vera' is not defined
     vera.natalia.main.py
 NameError: name 'vera' is not defined
 '''},
+{'date': 'Thu Oct 25 2018 21:29:46.942 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    from kathryn.main import Rodada
+ImportError: cannot import name 'Rodada'
+'''},
