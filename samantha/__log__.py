748f882f9c1e0c6df0dec6acdf5c027d7b6b16f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-09 18:35:08 -0200

    Created samantha/__log__.py @2018-11-09 18:35

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 09 2018 18:35:04.282 GMt-0200 (Brasilia Summer Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  game = new Phaser.Game(self.gameConfig);
+              ^
+SyntaxError: invalid syntax
+'''},
