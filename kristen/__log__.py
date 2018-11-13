6fa99f439f0ad20d749a5ba105fde32c5311e511
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-13 09:49:54 -0200

    Created kristen/__log__.py at Timestamp: 2018-11-13 11:49:54

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 13 2018 08:43:25.803 GMt-0200 (Brasilia Summer Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 45
+  puz.bind("click", lambda *_: doc["pydiv"].html = "")
+                                                 ^
+SyntaxError: can't assign to function call 
+'''},
