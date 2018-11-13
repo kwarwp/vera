6ecaa7de608f9322ff655be22187bfe17f63e4be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-13 11:32:31 -0200

    Created adda/__log__.py at Timestamp: 2018-11-13 13:32:31

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 13 2018 10:26:03.994 GMt-0200 (Brasilia Summer Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  def preload (self)
+                     ^
+SyntaxError: invalid syntax
+'''},
