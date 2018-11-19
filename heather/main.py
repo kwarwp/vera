ba4af453f194397b172908885e5cdbd93c88bbde
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-19 21:26:59 -0200

    Saved heather/main.py at Timestamp: 2018-11-19 23:26:59

--- heather/main.py
+++ heather/main.py
@@ -1,3 +1,63 @@
 # vera.heather.main.py
-SVG = """
-"""
+# vera.adda.main.py
+from browser import window, doc, alert
+from javascript import this as _this
+Phaser = window.Phaser
+class Main:
+    def __init__(self):
+        doc["pydiv"].html = ""
+        def preload():
+            
+            self.preload(_this())
+        def create(a):
+            self.create(_this())
+        self.config = dict(
+              type= Phaser.AUTO,
+              width= 800,
+              height= 600,
+              parent= "pydiv",
+              physics= {
+                        'default': 'arcade',
+                        'arcade': {
+                                 'gravity': { 'y': 200 }
+                                 }
+                        },
+              scene= {
+                      'preload': lambda *_: self.preload(_this()),
+                      'create': lambda *_: self.create(_this()),
+                      'update': lambda *_: self.update(_this())
+                      }
+              )
+
+        self.game = Phaser.Game.new(self.config)
+        window._game = self.game
+    def preload (self, _self):
+        this = _self
+        this.load.setBaseURL('http://labs.phaser.io')
+
+        this.load.image('sky', 'assets/skies/space3.png')
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+        this.load.image('red', 'assets/particles/red.png')
+
+    def create (self, _self):
+        this = _self
+        # this = self.game
+        this.add.image(400, 300, 'sky');
+        particles = this.add.particles('red');
+
+        emitter = particles.createEmitter({
+                                               'speed': 100,
+                                               'scale': { 'start': 1, 'end': 0 },
+                                               'blendMode': 'ADD'
+                                               });
+
+        logo = this.physics.add.image(400, 100, 'logo');
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(True);
+
+        emitter.startFollow(logo);
+
+if __name__ == "__main__":
+    Main()
