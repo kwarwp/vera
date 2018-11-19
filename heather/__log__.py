b7f2bd12812510599f54f1767784d15a0f90f037
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-19 21:33:28 -0200

    Created heather/__log__.py at Timestamp: 2018-11-19 23:33:28

---
+++ heather/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 19 2018 21:33:26.655 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 43
+  def create (self, this):
+  ^
+IndentationError: expected an indented block
+'''},
