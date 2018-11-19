188925e98885ae7e5ab8dead2b4fcfe8df12ef70
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-19 19:08:36 -0200

    Saved adda/__log__.py at Timestamp: 2018-11-19 21:08:36

--- adda/__log__.py
+++ adda/__log__.py
@@ -31,3 +31,21 @@ NameError: name 'width' is not defined
                                                                                                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 19 2018 19:08:35.486 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 54
+    Main()
+  module <module> line 6
+    doc[""].html = ""
+KeyError: 
+'''},
