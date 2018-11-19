939c21bb3d99f59f87d4a68819771af12c805b0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-19 21:38:12 -0200

    Saved heather/main.py at Timestamp: 2018-11-19 23:38:12

--- heather/main.py
+++ heather/main.py
@@ -33,30 +33,19 @@ class Main:
         window._game = self.game
     def preload (self, this):
         this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+        this.load.image('sky', 'assets/sky.png')
+        this.load.image('ground', 'assets/platform.png')
+        this.load.image('star', 'assets/star.png')
+        this.load.image('bomb', 'assets/bomb.png')
+        this.load.spritesheet('dude',  'assets/dude.png', { 'frameWidth': 32, 'frameHeight': 48 })
 
     def update (self, _self):
+        pass
 
     def create (self, this):
-        this.add.image(400, 300, 'sky');
-        particles = this.add.particles('red');
+        this.add.image(400, 300, 'sky')
+        this.add.image(400, 300, 'star')
 
-        emitter = particles.createEmitter({
-                                               'speed': 100,
-                                               'scale': { 'start': 1, 'end': 0 },
-                                               'blendMode': 'ADD'
-                                               });
-
-        logo = this.physics.add.image(400, 100, 'logo');
-
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(True);
-
-        emitter.startFollow(logo);
 
 if __name__ == "__main__":
     Main()
