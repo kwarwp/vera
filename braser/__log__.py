522092659a0d8947fb90605a05e06f70f497a9ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-20 11:29:43 -0200

    Created braser/__log__.py at Timestamp: 2018-11-20 13:29:43

---
+++ braser/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 20 2018 11:29:40.840 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  PHYSICS = dict(default='arcade', arcade=dict(gravity=dict(y=200))
+                                                                     ^
+SyntaxError: invalid syntax
+'''},
