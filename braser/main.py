0709163a7ee676f883a661c401166f85756eff2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-20 11:58:57 -0200

    Saved braser/main.py at Timestamp: 2018-11-20 13:58:56

--- braser/main.py
+++ braser/main.py
@@ -2,8 +2,10 @@
 from browser import window, doc, alert
 from javascript import this as _this
 Phaser = window.Phaser
-PHYSICS = dict(default='arcade', arcade=dict(gravity=dict(y=200))
-class Main:
+PHYSICS = dict(default='arcade', arcade=dict(gravity=dict(y=200)))
+class Braser:
+    game = None
+    subscribers = []
     def __init__(self, width=800, height=600, parent="pydiv", mode=Phaser.AUTO):
         scene= {
                     'preload': lambda *_: self._preload(_this()),
@@ -20,21 +22,77 @@ class Main:
               scene=scene
               )
 
-        self.game = Phaser.Game.new(self.config)
-        window._game = self.game
-    def _preload (self, _self):
-        this = _self
-        if this not in self.__base__:
-            self.__base__.append(this)
+        Braser.game = Phaser.Game.new(self.config) if not Braser.game else Braser.game
+
+    def subscribe(self, subscriber):
+        """
+        Subscribe elements for game loop.
+
+        :param subscriber:
+        """
+        Braser.subscribers.append(subscriber)
+        
+    def _preload (self, this):
+        self.game = this
+        self.preload()
+        for subscriber in Braser.subscribers:
+            subscriber.preload() if hasattr(subscriber, 'preload') else None
+
+    def _create (self, this):
+        self.game = this
+        self.create()
+        for subscriber in Braser.subscribers:
+            subscriber.create() if hasattr(subscriber, 'create') else None
+
+    def _update(self, this):
+        """
+        Update element.
+
+        """
+        self.update()
+        for subscriber in Braser.subscribers:
+            subscriber.update() if hasattr(subscriber, 'update') else None
+        
+    def preload (self, *_):
+        pass
+
+    def create (self, *_):
+        pass
+
+    def update(self, *_):
+        pass
+
+
+class Main(Braser):
+    """
+    Brython object-oriented  wrapper for js Phaser.
+
+    :param x: Canvas width.
+    :param y: Canvas height.
+    :param mode: Canvas mode.
+    :param name: Game name.
+    :param keyargs: Extra arguments
+    """
+
+    def __init__(self, x=800, y=600, mode=None, name="pydiv", **kwargs):
+        super().__init__()
+
+    def preload(self, *_):
+        """
+        Preload element.
+
+        """
         this.load.setBaseURL('http://labs.phaser.io')
 
         this.load.image('sky', 'assets/skies/space3.png')
         this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        self.load.image('red', 'assets/particles/red.png')
+        self.game.load.image('red', 'assets/particles/red.png')
 
-    def create (self, _self):
-        this = _self
-        # this = self.game
+    def create(self, *_):
+        """
+        Create element.
+
+        """
         this.add.image(400, 300, 'sky');
         particles = this.add.particles('red');
 
@@ -52,58 +110,6 @@ class Main:
 
         emitter.startFollow(logo);
 
+
 if __name__ == "__main__":
     Main()
-
-PHASER = window.Phaser
-BraserGame = PHASER.Game.new
-
-class Braser:
-    """
-    Brython object-oriented  wrapper for js Phaser.
-
-    :param x: Canvas width.
-    :param y: Canvas height.
-    :param mode: Canvas mode.
-    :param name: Game name.
-    :param keyargs: Extra arguments
-    """
-    PHASER = window.Phaser
-
-    def __init__(self, x=800, y=600, mode=None, name="pydiv", **kwargs):
-        mode = mode or PHASER.CANVAS
-        self.game = BraserGame(x, y, mode, name,
-                                {"preload": self.preload, "create": self.create, "update": self.update})
-        self.subscribers = []
-
-    def subscribe(self, subscriber):
-        """
-        Subscribe elements for game loop.
-
-        :param subscriber:
-        """
-        self.subscribers.append(subscriber)
-
-    def preload(self, *_):
-        """
-        Preload element.
-
-        """
-        for subscriber in self.subscribers:
-            subscriber.preload()
-
-    def create(self, *_):
-        """
-        Create element.
-
-        """
-        for subscriber in self.subscribers:
-            subscriber.create()
-
-    def update(self, *_):
-        """
-        Update element.
-
-        """
-        for subscriber in self.subscribers:
-            subscriber.update()
