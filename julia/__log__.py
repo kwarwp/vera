c494c2a5d6f897e233637a387ee557718f74a1f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-20 12:34:34 -0200

    Saved julia/__log__.py at Timestamp: 2018-11-20 14:34:33

--- julia/__log__.py
+++ julia/__log__.py
@@ -45,3 +45,25 @@ TypeError: 'module' object is not callable
     self.gamer = gamer.gamer
 AttributeError: 'Masmorra' object has no attribute 'gamer'
 '''},
+{'date': 'Tue Nov 20 2018 12:34:32.902 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 352
+    circus(0)
+  module <module> line 348
+    DES[desafio](param)
+  module <module> line 333
+    Masmorra()
+  module <module> line 26
+    self.gamer = Braser(800, 600, [self.Hero, self.Monster])
+AttributeError: 'Masmorra' object has no attribute 'Hero'
+'''},
