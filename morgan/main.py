c84437c08e8353c9f1a5c55c51c5682433c2e825
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:24:23 -0300

    Saved morgan/main.py at Timestamp: 2019-03-28 14:24:20

--- morgan/main.py
+++ morgan/main.py
@@ -1,6 +1,6 @@
 # vera.morgan.main.py
 "http://supygirls.pythonanywhere.com"
 from _spy.vitollino.main import Cena
-FLORESTA = https://i.imgur.com/wBw3Lyl.jpg
+FLORESTA = "https://i.imgur.com/wBw3Lyl.jpg"
 floresta_inicio = Cena(FLORESTA)
 floresta_inicio.vai()
