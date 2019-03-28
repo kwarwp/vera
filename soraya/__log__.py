42f665b3c198bb0e96c00c94577fdcc0e8a530a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:31:31 -0300

    Saved soraya/__log__.py at Timestamp: 2019-03-28 14:31:30

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -6,3 +6,19 @@
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Mar 28 2019 11:31:33.323 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    faca_Elemento(FACA)
+NameError: name 'faca_Elemento' is not defined
+'''},
