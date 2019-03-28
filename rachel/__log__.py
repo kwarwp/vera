1fba24b1d61f6a6e7a6a45d0cec1e4033ce37609
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 12:06:38 -0300

    Saved rachel/__log__.py at Timestamp: 2019-03-28 15:06:37

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -76,3 +76,21 @@ Traceback (most recent call last):
   module rachel.main line 4
     from amanda.main import FlorestaFaca
 '''},
+{'date': 'Thu Mar 28 2019 12:06:41.104 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    a_floresta = Florestabanana()
+  module <module> line 9
+    from amanda.main import FlorestaFaca
+ImportError: cannot import name 'FlorestaFaca'
+'''},
