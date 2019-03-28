d84594caf49101183dccab0d773f6896df8c84c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:30:04 -0300

    Created soraya/__log__.py at Timestamp: 2019-03-28 14:30:04

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Mar 28 2019 11:30:04.143 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  faca _ Elemento(FACA)
+        ^
+SyntaxError: invalid syntax
+'''},
