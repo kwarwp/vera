e3aead02188f41d50bf37d3762e0b3e9ff45fb9f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:30:39 -0300

    Saved rachel/main.py at Timestamp: 2019-03-28 14:30:38

--- rachel/main.py
+++ rachel/main.py
@@ -2,5 +2,8 @@
 "http://supygirls.pythonanywhere.com"
 from _spy.vitollino.main import Cena
 FLORESTA = "https://i.imgur.com/VHaolvA.jpg"
+BANANA = "https://i.imgur.com/RPrbLER.jpg"
 floresta_inicio = Cena(FLORESTA)
+banana = Elemento(BANANA)
+banana.entra(floresta_inicio)
 floresta_inicio.vai()
