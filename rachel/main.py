33216295d59d1bb2de18b40bdaaccb6560878b97
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:41:18 -0300

    Saved rachel/main.py at Timestamp: 2019-03-28 14:41:17

--- rachel/main.py
+++ rachel/main.py
@@ -4,6 +4,6 @@ from _spy.vitollino.main import Cena, Elemento
 FLORESTA = "https://i.imgur.com/VHaolvA.jpg"
 BANANA = "https://i.imgur.com/HnIHJd7.png"
 floresta_inicio = Cena(FLORESTA)
-banana = Elemento(BANANA)
+banana = Elemento(BANANA, style=dict(left="200px", width="80px"))
 banana.entra(floresta_inicio)
 floresta_inicio.vai()
