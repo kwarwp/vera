7fca646fd10e4fde53adeeb964afa1ed406002a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:55:24 -0300

    Saved rachel/main.py at Timestamp: 2019-03-28 14:55:23

--- rachel/main.py
+++ rachel/main.py
@@ -1,9 +1,18 @@
 # vera.rachel.main.py
 "http://supygirls.pythonanywhere.com"
 from _spy.vitollino.main import Cena, Elemento
+
 FLORESTA = "https://i.imgur.com/VHaolvA.jpg"
 BANANA = "https://i.imgur.com/HnIHJd7.png"
-floresta_inicio = Cena(FLORESTA)
-banana = Elemento(BANANA, style=dict(left="230px", width="50px"))
-banana.entra(floresta_inicio)
-floresta_inicio.vai()
+class Florestabanana:
+    def _init_(self):
+        self.floresta_inicio = Cena(FLORESTA)
+        banana = Elemento(BANANA, style=dict(left="230px", width="50px"))
+        banana.entra(self.floresta_inicio)
+        
+    def vai(self):
+        self.floresta_inicio.vai()
+        
+if __name__ == "__main__":
+    a_floresta = Florestabanana()
+    a_floresta.vai()
