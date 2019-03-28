7ebb8b7af1631b1cafaa286c31333968f2e1b62e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 12:02:28 -0300

    Saved morgan/main.py at Timestamp: 2019-03-28 15:02:28

--- morgan/main.py
+++ morgan/main.py
@@ -1,11 +1,14 @@
 # vera.morgan.main.py
 "http://supygirls.pythonanywhere.com"
 from _spy.vitollino.main import Cena, Elemento
+from rachel.main import Florestabanana
+
 FLORESTA = "https://i.imgur.com/wBw3Lyl.jpg"
 LEAO = "https://i.imgur.com/4gXpvfQ.png"
 class FlorestaLeao:
    def __init__(self):
-       self.floresta_inicio = Cena(FLORESTA)
+       floresta_banana = Florestabanana()
+       self.floresta_inicio = Cena(FLORESTA, direita=floresta_banana)
        leao = Elemento(LEAO, style=dict(left="150px",width="100px"))
        leao.entra(self.floresta_inicio)
 
