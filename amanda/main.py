0a8ff07a38f8479a549fb6faf68e9d14ed0f80bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:19:03 -0300

    Created amanda/main.py at Timestamp: 2019-03-28 14:19:03

---
+++ amanda/main.py
@@ -0,0 +1,6 @@
+# vera.amanda.main.py
+"http://supygirls.pythonanywhere.com"
+from _spy.vitollino.main import Cena
+FLORESTA = "https://i.imgur.com/vlJS7Ry.jpg"
+floresta_inicio = Cena(FLORESTA)
+floresta_inicio.vai()
