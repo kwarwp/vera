9c1dc2459eb2f0f07eaee259a4837e3df1a4d419
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 12:03:54 -0300

    Saved rachel/__log__.py at Timestamp: 2019-03-28 15:03:54

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -33,3 +33,46 @@ NameError: name 'Elemento' is not defined
     self.floresta_inicio.vai()
 AttributeError: 'Florestabanana' object has no attribute 'floresta_inicio'
 '''},
+{'date': 'Thu Mar 28 2019 12:03:57.271 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from amanda.main import FlorestaFaca
+  module amanda.main line 4
+    from morgan.main import FlorestaLeao
+  module morgan.main line 4
+    from rachel.main import Florestabanana
+  module rachel.main line 4
+    from amanda.main import FlorestaFaca
+ImportError: cannot import name 'FlorestaFaca'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from amanda.main import FlorestaFaca
+  module amanda.main line 4
+    from morgan.main import FlorestaLeao
+  module morgan.main line 4
+    from rachel.main import Florestabanana
+  module rachel.main line 4
+    from amanda.main import FlorestaFaca
+'''},
