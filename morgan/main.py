982a1243b4bf0bdca26fd83e66bc67a62ce98ba7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:37:19 -0300

    Saved morgan/main.py at Timestamp: 2019-03-28 14:37:19

--- morgan/main.py
+++ morgan/main.py
@@ -4,7 +4,7 @@ from _spy.vitollino.main import Cena, Elemento
 FLORESTA = "https://i.imgur.com/wBw3Lyl.jpg"
 LEAO = "https://i.imgur.com/4gXpvfQ.png"
 floresta_inicio = Cena(FLORESTA)
-leao = Elemento(LEAO, style=dict(width="200px"))
+leao = Elemento(LEAO, style=dict(width="150px"))
 floresta_inicio.vai()
 leao.entra(floresta_inicio)
 floresta_inicio.vai()
