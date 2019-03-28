491249831592fc98ca92a2be2f88216167e1d85d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:48:06 -0300

    Saved amanda/main.py at Timestamp: 2019-03-28 14:48:05

--- amanda/main.py
+++ amanda/main.py
@@ -1,9 +1,18 @@
 # vera.amanda.main.py
 "http://supygirls.pythonanywhere.com"
 from _spy.vitollino.main import Cena, Elemento
+
 FLORESTA = "https://i.imgur.com/vlJS7Ry.jpg"
-FACA = "https://i.imgur.com/pC6LXEZ.jpg"
-floresta_inicio = Cena(FLORESTA)
-faca = Elemento(FACA)
-faca.entra(floresta_inicio)
-floresta_inicio.vai()
+FACA = "https://i.imgur.com/H2vMcB4.png"
+class Floresta:
+    def __init__(self):
+        self.floresta_inicio = Cena(FLORESTA)
+        faca = Elemento(FACA, style=dict(left="200px", width="80px"))
+        faca.entra(self.floresta_inicio)
+        
+    def vai(self):
+        self.floresta_inicio.vai()
+        
+if __name__ == "__main__":
+    a_floresta = Floresta()
+    a_floresta.vai()
