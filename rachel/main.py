23df7dc52628ef9e0eec5af0295bb2493dd03060
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:19:53 -0300

    Created rachel/main.py at Timestamp: 2019-03-28 14:19:53

---
+++ rachel/main.py
@@ -0,0 +1,6 @@
+# vera.rachel.main.py
+"http://supygirls.pythonanywhere.com"
+from _spy.vitollino.main import Cena
+FLORESTA = "https://i.imgur.com/VHaolvA.jpg"
+floresta_inicio = Cena(FLORESTA)
+floresta_inicio.vai()
