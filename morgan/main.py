61d9dfea4e45b75e2e95a42ddecabc36b628b2ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:19:02 -0300

    Created morgan/main.py at Timestamp: 2019-03-28 14:19:01

---
+++ morgan/main.py
@@ -0,0 +1,6 @@
+# vera.morgan.main.py
+"http://supygirls.pythonanywhere.com"
+from _spy.vitollino.main import Cena
+FLORESTA = https://i.imgur.com/wBw3Lyl.jpg
+floresta_do_rio_inicio = Cena(FLORESTA)
+floresta_inicio.vai()
