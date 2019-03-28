c7842dd200c2a8b6e3d37af61b984ea4178a9c6f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:19:58 -0300

    Created morgan/__log__.py at Timestamp: 2019-03-28 14:19:58

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Mar 28 2019 11:19:57.794 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  FLORESTA = https://i.imgur.com/wBw3Lyl.jpg
+                   ^
+SyntaxError: invalid syntax
+'''},
