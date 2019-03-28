f680254cf35541ebf76dbbc71ef95276d1bf018c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-28 11:29:53 -0300

    Saved amanda/main.py at Timestamp: 2019-03-28 14:29:53

--- amanda/main.py
+++ amanda/main.py
@@ -1,6 +1,9 @@
 # vera.amanda.main.py
 "http://supygirls.pythonanywhere.com"
-from _spy.vitollino.main import Cena
+from _spy.vitollino.main import Cena, Elemento
 FLORESTA = "https://i.imgur.com/vlJS7Ry.jpg"
+FACA = "https://i.imgur.com/pC6LXEZ.jpg"
 floresta_inicio = Cena(FLORESTA)
+faca = Elemento(FACA)
+faca.entra(floresta_inicio)
 floresta_inicio.vai()
