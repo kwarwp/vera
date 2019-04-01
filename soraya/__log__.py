c4a9d4623c93cd07df7de67b219321a25a366758
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-01 07:17:14 -0300

    Saved soraya/__log__.py at Timestamp: 2019-04-01 10:17:14

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -29,3 +29,19 @@ NameError: name 'faca_Elemento' is not defined
                    ^
 SyntaxError: Unbalanced bracket (
 '''},
+{'date': 'Mon Apr 01 2019 07:17:07.185 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    a_florest = Florestamacaco()
+NameError: name 'Florestamacaco' is not defined
+'''},
