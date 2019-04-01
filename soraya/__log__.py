752e22e84af856447564e00909430ec5561d715b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-01 07:44:57 -0300

    Saved soraya/__log__.py at Timestamp: 2019-04-01 10:44:57

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -93,3 +93,19 @@ NameError: name 'Florestamacaco' is not defined
     a_floresta = Florestamacaco()
 NameError: name 'Florestamacaco' is not defined
 '''},
+{'date': 'Mon Apr 01 2019 07:44:51.156 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    macaco = Elemento(MACACO, style=dict(left="150px",width="50px"))
+NameError: name 'MACACO' is not defined
+'''},
