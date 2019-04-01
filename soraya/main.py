96eee801b1f3ded4d590b606df515663ac521ac7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-01 07:48:49 -0300

    Saved soraya/main.py at Timestamp: 2019-04-01 10:48:49

--- soraya/main.py
+++ soraya/main.py
@@ -5,7 +5,7 @@ from _spy.vitollino.main import Cena, Elemento
 FLORESTA = "https://i.imgur.com/bfO1gCN.jpg"
 macaco = "https://i.imgur.com/dIBkMcG.png"
 floresta_inicio = Cena(FLORESTA)
-macaco = Elemento(MACACO, style=dict(left="230px", width="50px"))
+macaco = Elemento(MACACO, style=dict(left="230px" , width="50px"))
 macaco.entra(self.floresta_inicio)
 floresta_inicio.vai()
         
