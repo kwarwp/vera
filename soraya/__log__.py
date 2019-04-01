aee06aba3feb9e476e5329357554adbb1dc59853
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-01 07:20:53 -0300

    Saved soraya/__log__.py at Timestamp: 2019-04-01 10:20:52

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -45,3 +45,19 @@ SyntaxError: Unbalanced bracket (
     a_florest = Florestamacaco()
 NameError: name 'Florestamacaco' is not defined
 '''},
+{'date': 'Mon Apr 01 2019 07:20:46.656 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    a_floresta = Florestamacaco()
+NameError: name 'Florestamacaco' is not defined
+'''},
