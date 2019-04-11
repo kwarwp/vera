c34f11a9bce16d6d96d401bac982f2f279ffaba2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-11 11:55:52 -0300

    Saved amanda/main.py at Timestamp: 2019-04-11 14:55:51

--- amanda/main.py
+++ amanda/main.py
@@ -1,13 +1,25 @@
 # vera.amanda.main.py
 "http://supygirls.pythonanywhere.com"
 from _spy.vitollino.main import Cena, Elemento
-from morgan.main import FlorestaLeao
-
+# from morgan.main import FlorestaLeao
 FLORESTA = "https://i.imgur.com/vlJS7Ry.jpg"
 FACA = "https://i.imgur.com/H2vMcB4.png"
+
+class CenaProxy:
+    def __init__(self, aqui=None):
+        self.aqui = aqui
+        self.floresta_leao = None
+    def vai(self):
+        from morgan.main import FlorestaLeao
+        self.floresta_leao = FlorestaLeao()
+        self.floresta_leao.esquerda = self.aqui
+        self.floresta_leao.vai()
+
 class FlorestaFaca:
     def __init__(self):
-        floresta_leao = FlorestaLeao()
+        # floresta_leao = FlorestaLeao() -XX- ERRO!
+        self.floresta_inicio = None
+        floresta_leao = CenaProxy(self.floresta_inicio)
         self.floresta_inicio = Cena(FLORESTA, direita=floresta_leao)
         faca = Elemento(FACA, style=dict(left="200px", width="80px"))
         faca.entra(self.floresta_inicio)
