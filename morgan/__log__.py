00c2db58e26e405817c3d1e67859208dd00ea69c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-11 12:05:48 -0300

    Saved morgan/__log__.py at Timestamp: 2019-04-11 15:05:47

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -102,3 +102,19 @@ NameError: name 'a_floresta_' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Apr 11 2019 12:05:58.908 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    a_floresta = Florestabanana()
+NameError: name 'Florestabanana' is not defined
+'''},
