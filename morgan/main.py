04bb4c01dfd94a312444279882aff9cc2426bde6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-11 12:04:08 -0300

    Saved morgan/main.py at Timestamp: 2019-04-11 15:04:08

--- morgan/main.py
+++ morgan/main.py
@@ -1,21 +1,32 @@
 # vera.morgan.main.py
 "http://supygirls.pythonanywhere.com"
 from _spy.vitollino.main import Cena, Elemento
-from rachel.main import Florestabanana
+# from morgan.main import FlorestaBanana
+FLORESTA = "https://i.imgur.com/vlJS7Ry.jpg"
+BANANA = "https://i.imgur.com/HnIHJd7.png"
 
-FLORESTA = "https://i.imgur.com/wBw3Lyl.jpg"
-LEAO = "https://i.imgur.com/4gXpvfQ.png"
-class FlorestaLeao:
-   def __init__(self):
-       floresta_banana = Florestabanana()
-       self.floresta_inicio = Cena(FLORESTA, direita=floresta_banana)
-       leao = Elemento(LEAO, style=dict(left="150px",width="100px"))
-       leao.entra(self.floresta_inicio)
+class CenaProxy:
+    def __init__(self, aqui=None):
+        self.aqui = aqui
+        self.floresta_banana = None
+    def vai(self):
+        from Rachel.main import FlorestaBanana
+        self.floresta_banana = FlorestaBanana()
+        self.floresta_Banana.esquerda = self.aqui
+        self.floresta_banana.vai()
 
+class FlorestaBanana:
+    def __init__(self):
+        # floresta_banana = FlorestaBanana() -XX- ERRO!
+        self.floresta_inicio = None
+        floresta_banana = CenaProxy(self.floresta_inicio)
+        self.floresta_inicio = Cena(FLORESTA, direita=floresta_banana)
+        banana = Elemento(BANANA, style=dict(left="200px", width="80px"))
+        banana.entra(self.floresta_inicio)
    def vai(self):
        self.floresta_inicio.vai()
 
 if __name__ == "__main__":
-   a_floresta = FlorestaLeao()
+   a_floresta = Florestabanana()
    a_floresta.vai()
 
