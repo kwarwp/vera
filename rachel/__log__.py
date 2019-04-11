e4607f5845192d71c09abf067f217844b2e8cafb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-11 12:06:32 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-11 15:06:31

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -105,3 +105,19 @@ RuntimeError: <InternalError: too much recursion>
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Apr 11 2019 12:06:30.393 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    a_floresta = FlorestaBanana()
+NameError: name 'FlorestaBanana' is not defined
+'''},
