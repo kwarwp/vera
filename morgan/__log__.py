f4d184f05455c0b5884ed5c1edd9d2586dc8a6ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-25 10:07:17 -0300

    Saved morgan/__log__.py at Timestamp: 2019-04-25 13:07:17

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -136,3 +136,19 @@ NameError: name 'Florestabanana' is not defined
     self.floresta_inicio = Cena(FLORESTA, direita=floresta_banana)
 NameError: name 'floresta_banana' is not defined
 '''},
+{'date': 'Thu Apr 25 2019 10:07:16.252 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    a_floresta = FlorestaBanana()
+NameError: name 'FlorestaBanana' is not defined
+'''},
