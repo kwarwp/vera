369885990506c8be7bc7374535cce9a689f149b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-25 11:22:59 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-25 14:22:58

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -142,3 +142,10 @@ SyntaxError: invalid syntax
                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Apr 25 2019 11:22:58.404 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 31
+  self.banana.vai = self.come:
+                              ^
+SyntaxError: invalid syntax
+'''},
