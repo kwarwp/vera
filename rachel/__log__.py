7f9a4427a6bbbe369a05d0e678373a15e7230fdf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-25 11:25:14 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-25 14:25:13

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -156,3 +156,23 @@ SyntaxError: invalid syntax
                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Apr 25 2019 11:25:12.585 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 49
+    a_floresta = FlorestaBanana()
+  module <module> line 43
+    banana = Banana(self.floresta_inicio)
+  module <module> line 24
+    self.fala = Texto(self.floresta_inicio, TEXTO_FACA)
+NameError: name 'TEXTO_FACA' is not defined
+'''},
