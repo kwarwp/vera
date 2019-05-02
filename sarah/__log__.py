0c29f1be0b1f0235019e9d4c994d51af81e79445
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-02 11:12:56 -0300

    Saved sarah/__log__.py at Timestamp: 2019-05-02 14:12:55

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -37,3 +37,23 @@ NameError: name 'floresta_leao' is not defined
     self.terra = Elemento(FACA, style=dict(left="200px", width="80px"), vai=self.pega)
 NameError: name 'FACA' is not defined
 '''},
+{'date': 'Thu May 02 2019 11:12:53.586 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 149
+    o_universo = Universo()
+  module <module> line 139
+    estacao = Estacao(self.universo)
+  module <module> line 107
+    self.icone = Elemento(icone, style=dict(left=left+50, top=top+50, width="50px", height="50px"), vai=self.pega)
+TypeError: Can't convert int to str implicitely
+'''},
