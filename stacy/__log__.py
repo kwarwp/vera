00ad1381af205c7c397df413b09e6ab25f2a1f8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-02 10:20:56 -0300

    Created stacy/__log__.py at Timestamp: 2019-05-02 13:20:56

---
+++ stacy/__log__.py
@@ -0,0 +1,21 @@
+
+{'date': 'Thu May 02 2019 10:20:52.658 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    o_universo = Universo()
+  module <module> line 123
+    estacao = Estacao(self.universo)
+  module <module> line 92
+    self.estacao = Elemento(ESTACAO, style=dict(left=left, top=top, width="200px"), vai=self.pega)
+NameError: name 'left' is not defined
+'''},
