95143cf3d91fdb12be7099a2ea88177bdac7c108
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-02 10:14:20 -0300

    Saved parisa/__log__.py at Timestamp: 2019-05-02 13:14:20

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -17,3 +17,21 @@
     estacao2 = Estacao(self.uiverso, left="400px", top="200px")
 AttributeError: 'Universo' object has no attribute 'uiverso'
 '''},
+{'date': 'Thu May 02 2019 10:14:19.673 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 132
+    o_universo = Universo()
+  module <module> line 125
+    estacao2 = Estacao(self.uiverso, left="400px", top="200px")
+AttributeError: 'Universo' object has no attribute 'uiverso'
+'''},
