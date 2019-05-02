079c26404c76b5c4f461025667dd2b7ad31978e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-02 11:31:03 -0300

    Saved stacy/__log__.py at Timestamp: 2019-05-02 14:31:01

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -55,3 +55,21 @@ UnboundLocalError: local variable 'foguete' referenced before assignment
     pessoaa = pessoa(self.universo, left="200px", top="50px")
 NameError: name 'pessoa' is not defined
 '''},
+{'date': 'Thu May 02 2019 11:31:00.850 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 163
+    o_universo = Universo()
+  module <module> line 156
+    pessoaa = Pessoa(self.universo, left="200px", top="50px")
+NameError: name 'Pessoa' is not defined
+'''},
