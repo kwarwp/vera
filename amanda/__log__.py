ebdc93095a7aae82961d33bf720c4bbab9f1b41f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-03 16:26:04 -0300

    Saved amanda/__log__.py at Timestamp: 2019-05-03 19:26:03

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -66,3 +66,23 @@ SyntaxError: invalid syntax
                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri May 03 2019 16:26:02.622 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 82
+    a_floresta = FlorestaFaca()
+  module <module> line 75
+    faca = Faca(self.floresta_inicio)
+  module <module> line 34
+    style = {'opacity': "inherited", 'width': 30, 'height': "30px", 'min-height': '30px', 'float': 'left',
+AttributeError: 'Faca' object has no attribute 'img'
+'''},
