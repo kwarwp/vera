fbd2705e3e4bc4ce7d7f0c69ee87c4583638ffd6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:45:54 -0300

    Saved julia/__log__.py at Timestamp: 2019-05-23 13:45:53

--- julia/__log__.py
+++ julia/__log__.py
@@ -83,3 +83,25 @@ AttributeError: 'Masmorra' object has no attribute 'Hero'
     peixe1 = Elemento(BICHO, x=100, y=100, w=60, h=40)
 NameError: name 'BICHO' is not defined
 '''},
+{'date': 'Thu May 23 2019 10:46:00.457 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    jogoPescaria()
+  module <module> line 11
+    pescaria = Cena(CENA_PESCARIA, x=500, y=500, w=250, h=200)
+  module _spy.vitollino.main line 778
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 821
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+AttributeError: 'int' object has no attribute '__getitem__'
+'''},
