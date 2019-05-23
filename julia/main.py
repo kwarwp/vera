a3ef79f10e257b35e3dfa2716e30666b98b3e8bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:21:04 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 13:21:03

--- julia/main.py
+++ julia/main.py
@@ -1,6 +1,6 @@
 # vera.julia.main.py
 from _spy.vitollino.main import Cena
 
-CENA_PESCARIA = "https://imgur.com/GX8p3JC"
+CENA_PESCARIA = "https://imgur.com/GX8p3JC.jpg"
 pescaria = Cena(CENA_PESCARIA)
 pescaria.vai()
