421d109ec6a6765f712b98d8a76ab9b591507b77
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:20:03 -0300

    Saved alexa/__log__.py at Timestamp: 2019-05-23 14:20:03

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -68,3 +68,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu May 23 2019 11:20:12.104 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  maca = Elemento(MACA, x=150, y=200, w=60, h=40)
+  ^
+IndentationError: expected an indented block
+'''},
