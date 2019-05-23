bc649c874f15499e103a06a67b87aaeb6d44427e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 12:03:53 -0300

    Saved julia/__log__.py at Timestamp: 2019-05-23 15:03:53

--- julia/__log__.py
+++ julia/__log__.py
@@ -250,3 +250,23 @@ SyntaxError: invalid syntax
                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu May 23 2019 12:04:00.935 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    jogoPescaria()
+  module <module> line 38
+    Peixe(pescaria)
+  module <module> line 16
+    peixe.entrapescaria
+AttributeError: 'Elemento' object has no attribute 'entrapescaria'
+'''},
