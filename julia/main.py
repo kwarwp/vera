52a73b2befe4beccf4c7c9d06c217188d7f2447a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:59:03 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 13:59:02

--- julia/main.py
+++ julia/main.py
@@ -3,17 +3,24 @@ from _spy.vitollino.main import Cena
 from elemento.main import Elemento #permite a movimentacao
 
 JARRA = "https://i.imgur.com/y2YyMOM.jpg"
-PEIXE1 = "https://i.imgur.com/4tDY3hD.png"
-PEIXE2 = "https://i.imgur.com/SfOAbaS.png"
+PEIXE = "https://i.imgur.com/4tDY3hD.png"
 CENA_PESCARIA = "https://imgur.com/GX8p3JC.jpg"
+
+class Peixe:
+    def __init__(self, pescaria):
+        peixe = Elemento(PEIXE, x=70, y=200, w=60, h=40)
+        peixe.entra(pescaria)
+        
+class Jarra:
+    def __init__(self, pescaria)
+        jarra = Elemento(JARRA, x=150, y=230, w=60, h=40)
+        jarra.entra(pescaria)
 
 class jogoPescaria:
     def __init__(self):
         pescaria = Cena(CENA_PESCARIA)
-        peixe1 = Elemento(PEIXE1, x=70, y=200, w=60, h=40)
-        peixe1.entra(pescaria)
-        peixe2=Elemento(PEIXE2, x=10, y=230, w=60, h=40)
-        peixe2.entra(pescaria)
+        peixe = Elemento(PEIXE, x=70, y=200, w=60, h=40)
+        peixe.entra(pescaria)
         jarra = Elemento(JARRA, x=150, y=230, w=60, h=40)
         jarra.entra(pescaria)
         jarra_b = Elemento(JARRA, x=230, y=230, w=60, h=40)
