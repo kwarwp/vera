501012feae0a4679c408fe76f648301c120a249a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:53:31 -0300

    Saved alexa/main.py at Timestamp: 2019-05-23 14:53:31

--- alexa/main.py
+++ alexa/main.py
@@ -9,32 +9,35 @@ BOLA = "https://i.imgur.com/5KDBA8Z.jpg"
 CENA_PARQUE = "https://i.imgur.com/kqWvogN.jpg"
 CRIANCA = "https://i.imgur.com/j0ETf5x.jpg"
 
-class JogoFruta:
-    def __init__(self): 
+class Fruta:
+    def __init__(self, parque, x=150, y=200):
         maca = Elemento(MACA, x=200, y=200, w=60, h=40)
         maca.entra(parque)
+        
 class Esportes:  
-    def __init__(self): 
+    def __init__(self, parque): 
         peteca = Elemento(PETECA, x=150, y=100, w=60, h=40)
         peteca.entra(parque)
         bola = Elemento(BOLA, x=100, y=200, w=80, h=60)
         bola.entra(parque)
+        
 class Bicho:
-    def __init__(self): 
+    def __init__(self, parque):
         coelho = Elemento(COELHO,  x=100, y=100, w=60, h=40)
         coelho.entra(parque)
+        
 class Crianca:
     def __init__(self, parque, tit="maria", x=0, y=210):
         crianca = Elemento(CRIANCA, tit=tit, x=x, y=y, w=70, h=140, style={"opacity": 0.3})
         crianca.entra(parque)
+        
 class Conjuntos:
     def __init__(self):
         parque = Cena(CENA_PARQUE)
         Bicho(parque)
         Crianca(parque)
         Crianca(parque, tit="esportes", y=100)
-        Crianca(parque, tit="frutas", x=80, y=100)
-        (parque)
+        Crianca(parque, tit="frutas", x=80, y=100)       
         Fruta(parque, x=230, y=230)
         parque.vai()
         
