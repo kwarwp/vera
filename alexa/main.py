1e796e5f336db938a12807b8acea76f391dbe894
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:19:32 -0300

    Saved alexa/main.py at Timestamp: 2019-05-23 14:19:32

--- alexa/main.py
+++ alexa/main.py
@@ -1,1 +1,25 @@
 # vera.alexa.main.py
+from _spy.vitollino.main import Cena
+from elemento.main import Elemento 
+MACA = "https://i.imgur.com/xzBftDW.jpg"
+COELHO = "https://i.imgur.com/yY22qoF.jpg"
+PETECA = "https://i.imgur.com/lkby3FK.jpg"
+CENA_PARQUE = "https://i.imgur.com/kqWvogN.jpg"
+CRIANCA = "https://i.imgur.com/j0ETf5x.jpg"
+
+class JogoFruta:
+    def __init__(self): 
+    maca = Elemento(MACA, x=140, y=200, w=60, h=40)
+        maca.entra(parque)
+class JogoEsportes:  
+    peteca = Elemento(PETECA, x=150, y=100, w=60, h=40)
+        peteca.entra(parque)
+class JogoBicho:        
+    coelho = Elemento(COELHO,  x=100, y=100, w=60, h=40)
+       coelho.entra(parque)
+class JogoConjunto:
+    def __init__(self):
+        parque = Cena(CENA_PARQUE)
+        
+parque.vai() 
+JogoConjuntos()
