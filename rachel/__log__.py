c79ee4abd2d12d34cd3c905f2cdea5740638a319
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:37:08 -0300

    Saved rachel/__log__.py at Timestamp: 2019-05-23 14:37:08

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -211,3 +211,23 @@ SyntaxError: invalid syntax
            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu May 23 2019 11:37:07.207 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 88
+    a_floresta = FlorestaBanana()
+  module <module> line 81
+    rede = Rede(self.floresta_inicio)
+  module <module> line 65
+    self.rede.entra(florestainicio)
+NameError: name 'florestainicio' is not defined
+'''},
