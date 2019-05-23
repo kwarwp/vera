10042ff881e63408a5a903c7c43f578570f7686b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 12:01:24 -0300

    Saved samantha/__log__.py at Timestamp: 2019-05-23 15:01:19

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -266,3 +266,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu May 23 2019 12:01:19.388 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 32
+  texto = Texto (self.parque, UMTEXTO)
+  ^
+IndentationError: expected an indented block
+'''},
