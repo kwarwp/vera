c4b1dcbede01474c559fe7dbbbfdccbdef3e7349
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:01:50 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 14:01:49

--- julia/main.py
+++ julia/main.py
@@ -12,19 +12,16 @@ class Peixe:
         peixe.entra(pescaria)
         
 class Jarra:
-    def __init__(self, pescaria):
-        jarra = Elemento(JARRA, x=150, y=230, w=60, h=40)
+    def __init__(self, pescaria, x=150, y=230):
+        jarra = Elemento(JARRA, x=x, y=y, w=60, h=40)
         jarra.entra(pescaria)
 
 class jogoPescaria:
     def __init__(self):
         pescaria = Cena(CENA_PESCARIA)
-        peixe = Elemento(PEIXE, x=70, y=200, w=60, h=40)
-        peixe.entra(pescaria)
-        jarra = Elemento(JARRA, x=150, y=230, w=60, h=40)
-        jarra.entra(pescaria)
-        jarra_b = Elemento(JARRA, x=230, y=230, w=60, h=40)
-        jarra_b.entra(pescaria)
+        Peixe(pescaria)
+        Jarra(pescaria)
+        Jarra(pescaria, x=230, y=230)
         pescaria.vai()
 
 jogoPescaria()
