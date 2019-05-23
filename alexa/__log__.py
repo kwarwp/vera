d7063416ab81adf4ed577f3ffe5f7f630a8e8e3a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:49:35 -0300

    Saved alexa/__log__.py at Timestamp: 2019-05-23 14:49:34

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -163,3 +163,19 @@ NameError: name 'JogoConjuntos' is not defined
     JogoConjuntos()
 NameError: name 'JogoConjuntos' is not defined
 '''},
+{'date': 'Thu May 23 2019 11:49:42.765 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    JogoConjuntos()
+NameError: name 'JogoConjuntos' is not defined
+'''},
