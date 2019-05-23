e736efc9caa4e9cc785e800e8adb36b0acba2dba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:52:12 -0300

    Saved alexa/__log__.py at Timestamp: 2019-05-23 14:52:11

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -231,3 +231,21 @@ TypeError: __init__() takes 1 positional argument but more were given
     Bicho(parque)
 TypeError: __init__() takes 1 positional argument but more were given
 '''},
+{'date': 'Thu May 23 2019 11:52:19.457 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    Conjuntos()
+  module <module> line 42
+    Fruta(parque, x=230, y=230)
+NameError: name 'Fruta' is not defined
+'''},
