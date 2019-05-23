6de969d1cd3b41d65e40442a2ee840084346efe9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:55:10 -0300

    Saved samantha/__log__.py at Timestamp: 2019-05-23 14:55:09

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -218,3 +218,23 @@ SyntaxError: invalid syntax
                         ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu May 23 2019 11:55:08.249 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    JogoLetra()
+  module <module> line 45
+    Crianca(parque, tit="joana")
+  module <module> line 26
+    escolhas = {"letra_a" : self.fala, "letra a": self.fala}
+AttributeError: 'Crianca' object has no attribute 'fala'
+'''},
