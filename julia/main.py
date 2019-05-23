9a072fc337980cf7f5f9cfe5337bb7507aedc279
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:50:33 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 13:50:33

--- julia/main.py
+++ julia/main.py
@@ -4,6 +4,7 @@ from elemento.main import Elemento #permite a movimentacao
 
 JARRA = "https://i.imgur.com/y2YyMOM.jpg"
 PEIXE1 = "https://i.imgur.com/Q1OaRaO.png"
+PEIXE2 = "https://i.imgur.com/SfOAbaS.png"
 CENA_PESCARIA = "https://imgur.com/GX8p3JC.jpg"
 
 class jogoPescaria:
@@ -11,6 +12,8 @@ class jogoPescaria:
         pescaria = Cena(CENA_PESCARIA)
         peixe1 = Elemento(PEIXE1, x=100, y=100, w=60, h=40)
         peixe1.entra(pescaria)
+        peixe2= Elemento(PEIXE2, x=100, y=100, w=60, h=40)
+        peixe2.entra(pescaria)
         jarra = Elemento(JARRA, x=150, y=200, w=60, h=40)
         jarra.entra(pescaria)
         jarra_b = Elemento(JARRA, x=230, y=230, w=60, h=40)
