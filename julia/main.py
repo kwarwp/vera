86c812383131c73d2782ce3e04faba98475601f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:09:13 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 14:09:12

--- julia/main.py
+++ julia/main.py
@@ -5,11 +5,17 @@ from elemento.main import Elemento #permite a movimentacao
 JARRA = "https://i.imgur.com/y2YyMOM.jpg"
 PEIXE = "https://i.imgur.com/4tDY3hD.png"
 CENA_PESCARIA = "https://imgur.com/GX8p3JC.jpg"
+CRIANCA = "https://i.imgur.com/sCOK0iT.png" #aproveitar os elementos da imagem, oval transparente, finge ser a crianca
 
 class Peixe:
     def __init__(self, pescaria):
         peixe = Elemento(PEIXE, x=70, y=200, w=60, h=40)
         peixe.entra(pescaria)
+
+class Crianca:
+    def __init__(self, pescaria):
+        crianca.Elemento(CRIANCA, 200, w=60, h=40)
+        crianca.entra(crianca)
         
 class Jarra:
     def __init__(self, pescaria, x=150, y=230):
@@ -22,6 +28,7 @@ class jogoPescaria:
         Peixe(pescaria)
         Jarra(pescaria)
         Jarra(pescaria, x=200, y=230)
+        Crianca
         pescaria.vai()
 
 jogoPescaria()
