191667917111f67513c0f8b400d1869adc547e27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:20:25 -0300

    Saved soraya/__log__.py at Timestamp: 2019-05-23 13:20:24

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -293,3 +293,21 @@ UnboundLocalError: local variable 'floresta_inicio' referenced before assignment
     a_floresta = FlorestaMacaco()
 TypeError: __init__() missing 1 positional argument: floresta_inicio
 '''},
+{'date': 'Thu May 23 2019 10:20:34.290 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    a_floresta = FlorestaMacaco()
+  module <module> line 21
+    floresta_faca = CenaProxy(self.floresta_inicio)
+AttributeError: 'FlorestaMacaco' object has no attribute 'floresta_inicio'
+'''},
