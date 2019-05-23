04915d7b68e02ad8694a28a6b5f5dcb415409b86
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:44:39 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 13:44:38

--- julia/main.py
+++ julia/main.py
@@ -5,11 +5,14 @@ from elemento.main import Elemento #permite a movimentacao
 JARRA = "https://i.imgur.com/y2YyMOM.jpg"
 PEIXE1 = "https://i.imgur.com/Q1OaRaO.png"
 CENA_PESCARIA = "https://imgur.com/GX8p3JC.jpg"
-pescaria = Cena(CENA_PESCARIA)
-peixe1 = Elemento(PEIXE1, x=100, y=100, w=60, h=40)
-peixe1.entra(pescaria)
-jarra = Elemento(JARRA, x=150, y=200, w=60, h=40)
-jarra.entra(pescaria)
-jarra_b = Elemento(JARRA, x=230, y=230, w=60, h=40)
-jarra_b.entra(pescaria)
-pescaria.vai()
+
+class jogoPescaria:
+    def __init__(self):
+        pescaria = Cena(CENA_PESCARIA)
+        peixe1 = Elemento(PEIXE1, x=100, y=100, w=60, h=40)
+        peixe1.entra(pescaria)
+        jarra = Elemento(JARRA, x=150, y=200, w=60, h=40)
+        jarra.entra(pescaria)
+        jarra_b = Elemento(JARRA, x=230, y=230, w=60, h=40)
+        jarra_b.entra(pescaria)
+        pescaria.vai()
