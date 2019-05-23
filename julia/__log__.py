86be06ddf682ddf65df57017e819fe303c652a45
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:18:14 -0300

    Saved julia/__log__.py at Timestamp: 2019-05-23 14:18:14

--- julia/__log__.py
+++ julia/__log__.py
@@ -180,3 +180,21 @@ NameError: name 'crianca' is not defined
                                                                         ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu May 23 2019 11:18:22.155 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    jogoPescaria()
+  module <module> line 31
+    Crianca(crianca)
+NameError: name 'crianca' is not defined
+'''},
