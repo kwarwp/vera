7f8161cbe49767f1221b9d5ecb7eec1b2b1dae32
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:43:53 -0300

    Saved samantha/__log__.py at Timestamp: 2019-05-23 13:43:53

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -31,3 +31,19 @@ SyntaxError: invalid syntax
            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu May 23 2019 10:43:52.42 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 11
+    letra_b = Elemento(letra_B, x=100, y=100, w=60, h=40)
+NameError: name 'letra_B' is not defined
+'''},
