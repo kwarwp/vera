ee844188840b126b918629093ae8a1ab22e7af8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:45:22 -0300

    Saved heather/main.py at Timestamp: 2019-05-23 14:45:21

--- heather/main.py
+++ heather/main.py
@@ -1,26 +1,27 @@
 # vera.heather.main.py
-from _spy.vitollino.main import Cena
+from _spy.vitollino.main import Cena, STYLE
 from elemento.main import Elemento
-
+STYLE["width"] = 600
+STYLE["height"] = "600px"
 JARRA = "https://i.imgur.com/gdiuQ9G.jpg"
 BICHO = "https://i.imgur.com/7JW6fup.jpg"
 CENA_PARQUE = "https://i.imgur.com/woKKIiA.jpg"
 CRIANCA = "https://i.imgur.com/siqpEvJ.png"
 
 class Crianca:
-    def __init__(self, parque, tit="maria", x=0, y=210):
-        crianca = Elemento(CRIANCA, tit=tit, x=x, y=y, w=70,
-        h=140, style={"opacity":0.3})
+    def __init__(self, parque, tit="maria", x=0, y=420):
+        crianca = Elemento(CRIANCA, tit=tit, x=x, y=y, w=140,
+        h=280, style={"opacity":0.3})
         crianca.entra(parque)
 
 class Esquilo:
     def __init__(self, parque):
-        bicho = Elemento(BICHO, x=190, y=180, w=60, h=40)
+        bicho = Elemento(BICHO, tit="esquilo", x=400, y=360, w=120, h=80)
         bicho.entra(parque)
 
 class Jarra:
-    def __init__(self, parque, x=150, y=200):
-        jarra = Elemento(JARRA, x=x, y=y, w=60, h=40)
+    def __init__(self, parque, tit="jarra de mel", x=300, y=400):
+        jarra = Elemento(JARRA, tit=tit, x=x, y=y, w=120, h=80)
         jarra.entra(parque)
 
 class JogoEsquilo:
@@ -28,10 +29,10 @@ class JogoEsquilo:
         parque = Cena(CENA_PARQUE)
         Esquilo(parque)
         Crianca(parque)
-        Crianca(parque, tit="julia", y=100)
-        Crianca(parque, tit="zeca", x=80, y=100)
+        Crianca(parque, tit="julia", y=200)
+        Crianca(parque, tit="zeca", x=160, y=200)
         Jarra(parque)
-        Jarra(parque, x=230, y=230)
+        Jarra(parque, tit="geleia de uva", x=460, y=460)
         parque.vai()
         
 JogoEsquilo()
