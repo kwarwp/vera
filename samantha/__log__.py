a6537e1fbf932ef449f21b01f7b8bc07e333ed4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:20:18 -0300

    Saved samantha/__log__.py at Timestamp: 2019-05-23 13:20:17

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -17,3 +17,10 @@ SyntaxError: invalid syntax
 'error': '''
 Exception: <TypeError: this.boot is undefined>
 '''},
+{'date': 'Thu May 23 2019 10:20:17.218 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 1
+  https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwizkOuE37HiAhWnLLkGHYABAZoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.istockphoto.com%2Fbr%2Fvetor%2Fbate-bate-divers%25C3%25A3o-gm165766336-16384085&psig=AOvVaw2fVyKEecYqm9f-0aMf24kF&ust=1558703992729972
+       ^
+SyntaxError: invalid syntax
+'''},
