b5a0d64693904f9b7221d040957fe7d972f6a94e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:17:53 -0300

    Saved samantha/__log__.py at Timestamp: 2019-05-23 14:17:53

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -131,3 +131,10 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu May 23 2019 11:17:53.117 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  crianca = Elemento(CRIANCA, tit="joana", x=x, y=y, w=70, h= 140, style={"opacity":0})
+  ^
+IndentationError: expected an indented block
+'''},
