59acec903a77b7b0b2b2fd2e9cb56940132486ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 11:18:43 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 14:18:43

--- julia/main.py
+++ julia/main.py
@@ -28,7 +28,7 @@ class jogoPescaria:
         Peixe(pescaria)
         Jarra(pescaria)
         Jarra(pescaria, x=200, y=230)
-        Crianca(crianca)
+        Crianca(pescaria)
         pescaria.vai()
 
 jogoPescaria()
