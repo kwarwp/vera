b4704792c80d3c9c8775d6bbf650d5f814559ae2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 10:38:30 -0300

    Saved julia/main.py at Timestamp: 2019-05-23 13:38:29

--- julia/main.py
+++ julia/main.py
@@ -1,6 +1,13 @@
 # vera.julia.main.py
 from _spy.vitollino.main import Cena
+from elemento.main import Elemento #permite a movimentacao
 
+JARRA = "https://i.imgur.com/y2YyMOM.jpg"
+PEIXE1 = "https://i.imgur.com/Q1OaRaO.png"
 CENA_PESCARIA = "https://imgur.com/GX8p3JC.jpg"
 pescaria = Cena(CENA_PESCARIA)
+peixe1 = Elemento(BICHO, x=100, y=100, w=60, h=40)
+peixe1.entra(pescaria)
+jarra = Elemento(JARRA)
+jarra.entra(pescaria)
 pescaria.vai()
