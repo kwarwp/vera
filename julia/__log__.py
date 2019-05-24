26629b37f702ee0ffe2cdbf2c0a941105b3e40ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 21:10:58 -0300

    Saved julia/__log__.py at Timestamp: 2019-05-24 00:10:58

--- julia/__log__.py
+++ julia/__log__.py
@@ -306,3 +306,23 @@ AttributeError: 'Peixe' object has no attribute 'peixedois'
     Jarra(pescaria)
 NameError: name 'Jarra' is not defined
 '''},
+{'date': 'Thu May 23 2019 21:10:56.966 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    jogoPescaria()
+  module <module> line 42
+    Balde(pescaria)
+  module <module> line 23
+    balde = Elemento(BALDE, tit=tit, x=x, y=y, w=100, h=100, style={"opacity": 0.3})
+NameError: name 'tit' is not defined
+'''},
