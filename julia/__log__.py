981d5924333cdf6a250f8ea29fb05e6516be6c1c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 21:14:22 -0300

    Saved julia/__log__.py at Timestamp: 2019-05-24 00:14:22

--- julia/__log__.py
+++ julia/__log__.py
@@ -326,3 +326,21 @@ NameError: name 'Jarra' is not defined
     balde = Elemento(BALDE, tit=tit, x=x, y=y, w=100, h=100, style={"opacity": 0.3})
 NameError: name 'tit' is not defined
 '''},
+{'date': 'Thu May 23 2019 21:14:20.971 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    jogoPescaria()
+  module <module> line 42
+    Balde(pescaria, x=20, y=400, w=10, h=10)
+TypeError: __init__() got an unexpected keyword argument 'w'
+'''},
