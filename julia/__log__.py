3f3ea435dc14002b60a7e655b5bac2b16fc4a248
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-23 21:17:02 -0300

    Saved julia/__log__.py at Timestamp: 2019-05-24 00:17:01

--- julia/__log__.py
+++ julia/__log__.py
@@ -344,3 +344,21 @@ NameError: name 'tit' is not defined
     Balde(pescaria, x=20, y=400, w=10, h=10)
 TypeError: __init__() got an unexpected keyword argument 'w'
 '''},
+{'date': 'Thu May 23 2019 21:17:00.270 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    jogoPescaria()
+  module <module> line 37
+    Peixe(pescaria, x=200, y=400)
+TypeError: __init__() got an unexpected keyword argument 'x'
+'''},
