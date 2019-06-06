5ac9a15cffea5c260ce48b13701551607adc787a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 11:01:09 -0300

    Saved alexa/main.py at Timestamp: 2019-06-06 14:01:08

--- alexa/main.py
+++ alexa/main.py
@@ -16,18 +16,18 @@ TENIS = "https://i.imgur.com/CZBRVIk.png"
 GALOCHA = "https://i.imgur.com/9JmKVA4.jpg"
 
 class Fruta:
-    def __init__(self, parque,tit="maçã", imagem=MACA, x=150, y=200):
+    def __init__(self, parque,tit="maçã", imagem=MACA, x=200, y=200):
         maca = Elemento(imagem, tit=tit, x=x, y=y, w=60, h=40)
         maca.entra(parque)
 class Esportes:  
     def __init__(self, parque,tit="bola", imagem=BOLA, x=100, y=200): 
-        peteca = Elemento(imagem, tit=tit, x=x, y=y, w=50, h=40)
+        peteca = Elemento(imagem, tit=tit, x=x, y=y, w=40, h=50)
         peteca.entra(parque)
         #bola = Elemento(BOLA, x=100, y=200, w=80, h=60)
         #bola.entra(parque)
         
 class Bicho:
-    def __init__(self, parque, tit="coelho", imagem=COELHO, x=200, y=100):
+    def __init__(self, parque, tit="coelho", imagem=COELHO, x=30, y=40):
         #coelho = Elemento(COELHO, x=100, y=100, w=60, h=40)
         #coelho.entra(parque)
         passarinho = Elemento(imagem, tit=tit, x=x, y=y, w=70, h=70)
@@ -53,11 +53,11 @@ class Conjuntos:
         Crianca(parque, tit="frutas", x=80, y=100) 
         Crianca(parque, tit="calcado", y=200)
         Fruta(parque, x=230, y=230)
-        Fruta(parque, tit="laranja", imagem=LARANJA, x=330, y=230)
-        Esportes(parque, x=200, y=150)
-        Esportes(parque, tit="peteca", imagem=PETECA, x=250, y=200) 
-        Bicho(parque, x=400, y=450)
-        Bicho(parque, tit="passarinho", imagem=PASSARINHO, x=280, y=200)
+        Fruta(parque, tit="laranja", imagem=LARANJA, x=330, y=90)
+        Esportes(parque, x=100, y=150)
+        Esportes(parque, tit="peteca", imagem=PETECA, x=300, y=30) 
+        Bicho(parque, x=100, y=40)
+        Bicho(parque, tit="passarinho", imagem=PASSARINHO, x=400, y=60)
         Calcado(parque, x=250, y=280) 
         Calcado(parque, tit="galocha", imagem=GALOCHA, x=350, y=320) 
         parque.vai()
