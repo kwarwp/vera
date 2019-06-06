4204349633dc2f3ecfc85068acfeb1b61fc6cc17
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 09:39:14 -0300

    Created cobra/__log__.py at Timestamp: 2019-06-06 12:39:14

---
+++ cobra/__log__.py
@@ -0,0 +1,21 @@
+
+{'date': 'Thu Jun 06 2019 09:40:03.51 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 95
+    a_floresta = FlorestaBanana()
+  module <module> line 86
+    banana = Banana(self.floresta_inicio)
+  module <module> line 32
+    self.fala = Texto(self.floresta_inicio, TEXTO_BANANA)
+NameError: name 'TEXTO_BANANA' is not defined
+'''},
