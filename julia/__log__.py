18d168c619bf990d5bfa311a25ee4226d79b5dc4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 10:24:32 -0300

    Saved julia/__log__.py at Timestamp: 2019-06-06 13:24:31

--- julia/__log__.py
+++ julia/__log__.py
@@ -411,3 +411,23 @@ SyntaxError: invalid syntax
             ^
 SyntaxError: keyword can't be an expression
 '''},
+{'date': 'Thu Jun 06 2019 10:24:31.34 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    jogoPescaria()
+  module <module> line 115
+    Peixe5(pescaria, x=420, y=470)
+  module <module> line 47
+    peixe = Elemento(PEIXEAZULROXO, x=x, y=y, w=60, h=40, drag=True)
+NameError: name 'PEIXEAZULROXO' is not defined
+'''},
