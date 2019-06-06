da91b5ffad92a100ed7f18193520aa2750799405
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 09:55:33 -0300

    Saved stacy/__log__.py at Timestamp: 2019-06-06 12:55:33

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -169,3 +169,21 @@ SyntaxError: invalid syntax
                                                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jun 06 2019 09:55:45.451 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 167
+    o_universo = Universo()
+  module <module> line 156
+    estacao = Estacao(self.universo_Estacao.esquerda , left="80px", top="250px", icone=PESSOA)
+AttributeError: 'Universo' object has no attribute 'universo_Estacao'
+'''},
