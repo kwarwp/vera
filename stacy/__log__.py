bce44d84dab83720ddcacfba287661a97cca3d37
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 10:08:48 -0300

    Saved stacy/__log__.py at Timestamp: 2019-06-06 13:08:48

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -230,3 +230,21 @@ TypeError: __init__() got an unexpected keyword argument 'left'
     estacao = Estacao(self.universo, left="250px", width="400px", top="300px", icone=PESSOA)
 TypeError: __init__() got an unexpected keyword argument 'width'
 '''},
+{'date': 'Thu Jun 06 2019 10:09:00.296 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 166
+    o_universo = Universo()
+  module <module> line 155
+    estacao = Estacao(self.universo, left="250px", top="300px", width="400px", icone=PESSOA)
+TypeError: __init__() got an unexpected keyword argument 'width'
+'''},
