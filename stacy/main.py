c97159fd1fc3952cf7b6235039769061ed102be1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 10:26:23 -0300

    Saved stacy/main.py at Timestamp: 2019-06-06 13:26:22

--- stacy/main.py
+++ stacy/main.py
@@ -63,7 +63,7 @@ class Terra:
         self.falou = Texto(self.universo, FACA_FOI)
         self.usar = Texto(self.universo, FACA_USA)
         """
-        self.terra = Elemento(TERRA, x=800, y=250, w=320, h=280, vai=self.pega)
+        self.terra = Elemento(TERRA, x=800, y=290, w=320, h=280, vai=self.pega)
         self.longe = Cena()
         self.na_mao = False
         self.terra.entra(self.universo)
