e2a992e8f5d8cdb0193426ef9e9b358d73615208
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 10:31:03 -0300

    Saved stacy/main.py at Timestamp: 2019-06-06 13:31:01

--- stacy/main.py
+++ stacy/main.py
@@ -88,7 +88,7 @@ class Terra:
 class Foguete:
     def __init__(self, universo, left="300px", top="300px"):
         self.universo = universo
-        self.foguete = Elemento(FOGUETE, style=dict(left=left, top=top, width="80px", height="200px"), vai=self.pega)
+        self.foguete = Elemento(FOGUETE, style=dict(left=left, top=top, width="800px", height="200px"), vai=self.pega)
         self.longe = Cena()
         self.na_mao = False
         self.foguete.entra(self.universo)
