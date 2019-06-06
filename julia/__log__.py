d6ed1cfc6cdd63873a0965b1e3165ab55938b13c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 10:26:57 -0300

    Saved julia/__log__.py at Timestamp: 2019-06-06 13:26:56

--- julia/__log__.py
+++ julia/__log__.py
@@ -471,3 +471,23 @@ NameError: name 'PEIXEAZULROXO' is not defined
     peixe = Elemento(PEIXESEIS, x=x, y=y, w=60, h=40, drag=True)
 NameError: name 'PEIXESEIS' is not defined
 '''},
+{'date': 'Thu Jun 06 2019 10:26:55.685 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 132
+    jogoPescaria()
+  module <module> line 117
+    Peixe8(pescaria, x=390, y=505)
+  module <module> line 61
+    peixe = Elemento(PEIXE_AZUL, x=x, y=y, w=60, h=40, drag=True)
+NameError: name 'PEIXE_AZUL' is not defined
+'''},
