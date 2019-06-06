2bd3f031cb1f8c50b9dbf0ddfc7e55a263548009
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-06 10:49:21 -0300

    Saved alexa/main.py at Timestamp: 2019-06-06 13:49:20

--- alexa/main.py
+++ alexa/main.py
@@ -21,13 +21,13 @@ class Fruta:
         maca.entra(parque)
 class Esportes:  
     def __init__(self, parque,tit="bola", imagem=BOLA, x=100, y=200): 
-        peteca = Elemento(imagem, tit=tit, x=x, y=y, w=50, h=40)
+        peteca = Elemento(imagem, tit=tit, x=x, y=y, w=40, h=40)
         peteca.entra(parque)
         #bola = Elemento(BOLA, x=100, y=200, w=80, h=60)
         #bola.entra(parque)
         
 class Bicho:
-    def __init__(self, parque, tit="coelho", imagem=COELHO, x=200, y=100):
+    def __init__(self, parque, tit="coelho", imagem=COELHO, x=100, y=300):
         #coelho = Elemento(COELHO, x=100, y=100, w=60, h=40)
         #coelho.entra(parque)
         passarinho = Elemento(imagem, tit=tit, x=x, y=y, w=70, h=70)
@@ -55,7 +55,7 @@ class Conjuntos:
         Fruta(parque, x=230, y=230)
         Fruta(parque, tit="laranja", imagem=LARANJA, x=330, y=230)
         Esportes(parque, x=200, y=150)
-        Esportes(parque, tit="peteca", imagem=PETECA, x=250, y=200) 
+        Esportes(parque, tit="peteca", imagem=PETECA, x=400, y=200) 
         Bicho(parque, x=400, y=450)
         Bicho(parque, tit="passarinho", imagem=PASSARINHO, x=280, y=200)
         Calcado(parque, x=250, y=280) 
