474a60aca89161d06b54175f51e5f3385db1aae3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-13 11:16:49 -0300

    Saved mapa/__log__.py at Timestamp: 2019-06-13 14:16:49

--- mapa/__log__.py
+++ mapa/__log__.py
@@ -17,3 +17,21 @@
     pescaria = Cena(CENA_PESCARIA)
 NameError: name 'CENA_PESCARIA' is not defined
 '''},
+{'date': 'Thu Jun 13 2019 11:16:52.876 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    mapaParque()
+  module <module> line 11
+    INVENTARIO.inicia()
+NameError: name 'INVENTARIO' is not defined
+'''},
