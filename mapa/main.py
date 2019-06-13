e39396f782a2535e0e9ed53a62c144e0c18724ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-13 12:00:25 -0300

    Saved mapa/main.py at Timestamp: 2019-06-13 15:00:21

--- mapa/main.py
+++ mapa/main.py
@@ -1,5 +1,5 @@
 # vera.mapa.main.py
-from _spy.vitollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, STYLE, Elemento
 from elemento.main import Elemento
 STYLE["width"] = 900
 STYLE["heigth"] = "900px"
@@ -7,11 +7,27 @@ STYLE["heigth"] = "900px"
 CENA_MAPA = "https://i.imgur.com/4d2spVj.jpg"
 CLICAR = "https://i.imgur.com/U8kaaKl.jpg"
 
+class CenaProxy:
+    def __init__(self, aqui=None):
+        self.aqui = aqui
+        self.mapaSeleciona = None 
+    def vai(self, ev):
+        from julia.main import jogoPescaria
+        jogoPescaria.vai()
+        
 
+class area:
+    def __init__(self, mapa, tit="area", x=350, y=200):
+        clicar = Elemento(CLICAR, tit=tit, x=x, y=y, w=100,h=100, style={"opacity":0.3})
+        clicar.entra(mapa)
+        clicar.vai=CenaProxy().vai
+        
 class mapaParque:
     def __init__(self):
         mapa = Cena(CENA_MAPA)
         mapa.vai()
+        area(mapa, tit="pescaria", x=150, y=280)
         
+                
 mapaParque()
       
