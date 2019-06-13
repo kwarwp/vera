68b53f695138b5076f9ab3cc503d24aa9d5c2b9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-13 11:33:28 -0300

    Saved mar/main.py at Timestamp: 2019-06-13 14:33:27

--- mar/main.py
+++ mar/main.py
@@ -5,5 +5,5 @@ from elemento.main import Elemento
 PRAIA = "https://i.imgur.com/slKTEBc.png"
 ILHAMAR = "https://i.imgur.com/3FUDzbn.jpg"
 ilhamar = Cena(ILHAMAR)
-praia = Elemento(PRAIA, cena=ilhamar, x=500, y=400, h=700, style=dict(width= 700, height="650px"))
+praia = Elemento(PRAIA, cena=ilhamar, x=700, y=375, h=900, style=dict(width= 500, height="650px"))
 ilhamar.vai()
