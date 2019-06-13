c604dcd8194f99c0a86fdbb4acc6885fc900d1f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-13 18:24:10 -0300

    Saved mapa/__log__.py at Timestamp: 2019-06-13 21:24:09

--- mapa/__log__.py
+++ mapa/__log__.py
@@ -77,3 +77,21 @@ SyntaxError: invalid syntax
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jun 13 2019 18:24:07.204 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 61
+    mapaParque()
+  module <module> line 56
+    area(mapa, tit="pescaria", x=150, y=280)
+NameError: name 'area' is not defined
+'''},
