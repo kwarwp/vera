41cf4592c27857e5574c4830d591c189719a4d27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-13 11:55:33 -0300

    Saved mar/main.py at Timestamp: 2019-06-13 14:55:32

--- mar/main.py
+++ mar/main.py
@@ -4,6 +4,8 @@ STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 PRAIA = "https://i.imgur.com/slKTEBc.png"
 ILHAMAR = "https://i.imgur.com/3FUDzbn.jpg"
+BARCO = "https://i.imgur.com/ckjd6cI.png"
 ilhamar = Cena(ILHAMAR)
 praia = Elemento(PRAIA, cena=ilhamar, x=700, y=375, h=900, style=dict(width= 500, height="650px"))
 ilhamar.vai()
+barco = Elemento(BARCO, cena=ilhamar, x=300, y=450, h=900, style=dict(width= 100, height="650px"))
