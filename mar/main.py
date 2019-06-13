17241ddf1fe38ee78daf7252a500cb6b26b3d7ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-13 11:25:37 -0300

    Saved mar/main.py at Timestamp: 2019-06-13 14:25:36

--- mar/main.py
+++ mar/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, STYLE
 STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 PRAIA = "https://i.imgur.com/slKTEBc.png"
-ILHAMAR = "https://i.imgur.com/YaIP847.jpg"
+ILHAMAR = "https://i.imgur.com/3FUDzbn.jpg"
 ilhamar = Cena(ILHAMAR)
 praia = Elemento(PRAIA, cena=ilhamar, x=500, y=400, h=700, style=dict(width= 700, height="650px"))
 ilhamar.vai()
