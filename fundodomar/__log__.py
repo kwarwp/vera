1dba26676ac4acf538342f7b73da7d8743944a8c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 10:27:54 -0300

    Created fundodomar/__log__.py at Timestamp: 2019-06-27 13:27:53

---
+++ fundodomar/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Thu Jun 27 2019 10:28:25.977 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    fundodomar = Elemento(FUNDODOMAR, cena=fundodomar, x=700, y=398, h=900, style=dict(width= 500, height="650px"))
+NameError: name 'fundodomar' is not defined
+'''},
