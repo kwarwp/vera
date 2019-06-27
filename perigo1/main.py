494792687d48f19cb84920b54106b56e76a39318
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 09:37:40 -0300

    Created perigo1/main.py at Timestamp: 2019-06-27 12:37:40

---
+++ perigo1/main.py
@@ -0,0 +1,21 @@
+# vera.perigo1.main.py
+# vera.mar.main.py
+from _spy.vitollino.main import Cena, STYLE
+STYLE["width"], STYLE["height"] = 1200, "650px"
+from elemento.main import Elemento
+PRAIA = "https://i.imgur.com/slKTEBc.png"
+ILHAMAR = "https://i.imgur.com/3FUDzbn.jpg"
+BOIA = "https://i.imgur.com/GmzOUTL.png"
+ilhamar = Cena(ILHAMAR)
+praia = Elemento(PRAIA, cena=ilhamar, x=700, y=398, h=900, style=dict(width= 500, height="650px"))
+ilhamar.vai()
+boia = Elemento(BOIA, cena=ilhamar, x=30, y=440, h=900, style=dict(width= 30, height="650px"))
+boia2 = Elemento(BOIA, cena=ilhamar, x=100, y=500, h=900, style=dict(width= 30, height="650px"))
+boia3 = Elemento(BOIA, cena=ilhamar, x=200, y=550, h=900, style=dict(width= 30, height="650px"))
+boia4 = Elemento(BOIA, cena=ilhamar, x=320, y=600, h=900, style=dict(width= 30, height="650px"))
+boia5 = Elemento(BOIA, cena=ilhamar, x=440, y=620, h=900, style=dict(width= 30, height="650px"))
+boia6 = Elemento(BOIA, cena=ilhamar, x=150, y=450, h=900, style=dict(width= 30, height="650px"))
+boia7 = Elemento(BOIA, cena=ilhamar, x=300, y=490, h=900, style=dict(width= 30, height="650px"))
+boia8 = Elemento(BOIA, cena=ilhamar, x=450, y=530, h=900, style=dict(width= 30, height="650px"))
+boia9 = Elemento(BOIA, cena=ilhamar, x=600, y=570, h=900, style=dict(width= 30, height="650px"))
+boia10 = Elemento(BOIA, cena=ilhamar, x=750, y=600, h=900, style=dict(width= 30, height="650px"))
