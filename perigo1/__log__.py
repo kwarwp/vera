72f17b2012280e2b7f666f2b03eb3a4c13d5a2ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 11:48:15 -0300

    Saved perigo1/__log__.py at Timestamp: 2019-06-27 14:48:15

--- perigo1/__log__.py
+++ perigo1/__log__.py
@@ -13,3 +13,19 @@ SyntaxError: Unbalanced bracket (
                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jun 27 2019 11:48:50.83 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    tubarao = Elemento(TUBARAO, cena=ilhamar, x=700, y= 500, w=150, style={"opacity":0.2}, vai=tub)
+NameError: name 'tub' is not defined
+'''},
