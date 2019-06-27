4a27df1c737edd1ac9c493106a1d08c2fe21becf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 11:34:20 -0300

    Created perigo1/__log__.py at Timestamp: 2019-06-27 14:34:20

---
+++ perigo1/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jun 27 2019 11:34:54.69 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  redemoinho = Elemento(REDEMOINHO, cena=ilhamar, x=30, y=70, w=100
+                       ^
+SyntaxError: Unbalanced bracket (
+'''},
