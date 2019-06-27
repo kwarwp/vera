43135c51dbcd1f44c6e41502ba9d9ae2c585fde4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 11:45:56 -0300

    Saved perigo1/__log__.py at Timestamp: 2019-06-27 14:45:55

--- perigo1/__log__.py
+++ perigo1/__log__.py
@@ -6,3 +6,10 @@
                        ^
 SyntaxError: Unbalanced bracket (
 '''},
+{'date': 'Thu Jun 27 2019 11:46:30.928 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  tubarao = Elemento(TUBARAO, cena=ilhamar, x=700, y= 500, w=150, style={"opacity"=0.2})
+                                                                                       ^
+SyntaxError: invalid syntax
+'''},
