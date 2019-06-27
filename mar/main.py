f51bd5f80a949eada35083ddaf50f26159526b77
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 08:48:26 -0300

    Saved mar/main.py at Timestamp: 2019-06-27 11:48:26

--- mar/main.py
+++ mar/main.py
@@ -5,7 +5,13 @@ from elemento.main import Elemento
 PRAIA = "https://i.imgur.com/slKTEBc.png"
 ILHAMAR = "https://i.imgur.com/3FUDzbn.jpg"
 BARCO = "https://i.imgur.com/ckjd6cI.png"
+BOIA = "https://i.imgur.com/GmzOUTL.png"
 ilhamar = Cena(ILHAMAR)
 praia = Elemento(PRAIA, cena=ilhamar, x=700, y=398, h=900, style=dict(width= 500, height="650px"))
 ilhamar.vai()
 barco = Elemento(BARCO, cena=ilhamar, x=300, y=440, h=500, style=dict(width= 80, height="650px"))
+boia = Elemento(BOIA, cena=ilhamar, x=100, y=450, h=900, style=dict(width= 50, height="650px"))
+boia2 = Elemento(BOIA, cena=ilhamar, x=200, y=500, h=900, style=dict(width= 50, height="650px"))
+boia3 = Elemento(BOIA, cena=ilhamar, x=300, y=550, h=900, style=dict(width= 50, height="650px"))
+boia4 = Elemento(BOIA, cena=ilhamar, x=400, y=600, h=900, style=dict(width= 50, height="650px"))
+boia5 = Elemento(BOIA, cena=ilhamar, x=600, y=600, h=900, style=dict(width= 50, height="650px"))
