16d1eb639dffd36c41ceca0ce67672a1ee42807d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 12:01:42 -0300

    Saved soraya/__log__.py at Timestamp: 2019-06-27 15:01:41

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -345,3 +345,19 @@ NameError: name 'Texto' is not defined
     STYLE["width"] = 1200
 AttributeError: 'module' object has no attribute '__setitem__'
 '''},
+{'date': 'Thu Jun 27 2019 12:01:43.761 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    STYLE["width"] = 1200
+AttributeError: 'module' object has no attribute '__setitem__'
+'''},
