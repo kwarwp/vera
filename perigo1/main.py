f00a25de24c3de771a892035c55cc7fa2c17ade2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-27 09:52:17 -0300

    Saved perigo1/main.py at Timestamp: 2019-06-27 12:52:16

--- perigo1/main.py
+++ perigo1/main.py
@@ -4,18 +4,18 @@ from _spy.vitollino.main import Cena, STYLE
 STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 PRAIA = "https://i.imgur.com/slKTEBc.png"
-ILHAMAR = "https://i.imgur.com/3FUDzbn.jpg"
+ILHAMAR = "https://i.imgur.com/ifRjzot.png"
 BOIA = "https://i.imgur.com/GmzOUTL.png"
 ilhamar = Cena(ILHAMAR)
-praia = Elemento(PRAIA, cena=ilhamar, x=700, y=398, h=900, style=dict(width= 500, height="650px"))
+# praia = Elemento(PRAIA, cena=ilhamar, x=700, y=398, h=900, style=dict(width= 500, height="650px"))
 ilhamar.vai()
-boia = Elemento(BOIA, cena=ilhamar, x=30, y=440, h=900, style=dict(width= 30, height="650px"))
-boia2 = Elemento(BOIA, cena=ilhamar, x=100, y=500, h=900, style=dict(width= 30, height="650px"))
-boia3 = Elemento(BOIA, cena=ilhamar, x=200, y=550, h=900, style=dict(width= 30, height="650px"))
-boia4 = Elemento(BOIA, cena=ilhamar, x=320, y=600, h=900, style=dict(width= 30, height="650px"))
-boia5 = Elemento(BOIA, cena=ilhamar, x=440, y=620, h=900, style=dict(width= 30, height="650px"))
-boia6 = Elemento(BOIA, cena=ilhamar, x=150, y=450, h=900, style=dict(width= 30, height="650px"))
-boia7 = Elemento(BOIA, cena=ilhamar, x=300, y=490, h=900, style=dict(width= 30, height="650px"))
-boia8 = Elemento(BOIA, cena=ilhamar, x=450, y=530, h=900, style=dict(width= 30, height="650px"))
-boia9 = Elemento(BOIA, cena=ilhamar, x=600, y=570, h=900, style=dict(width= 30, height="650px"))
-boia10 = Elemento(BOIA, cena=ilhamar, x=750, y=600, h=900, style=dict(width= 30, height="650px"))
+boia = Elemento(BOIA, cena=ilhamar, x=30, y=140, w=50)
+boia2 = Elemento(BOIA, cena=ilhamar, x=100, y=200, w=50)
+boia3 = Elemento(BOIA, cena=ilhamar, x=200, y=250, w=50)
+boia4 = Elemento(BOIA, cena=ilhamar, x=320, y=300, w=50)
+boia5 = Elemento(BOIA, cena=ilhamar, x=440, y=320, w=50)
+boia6 = Elemento(BOIA, cena=ilhamar, x=150, y=150, w=50)
+boia7 = Elemento(BOIA, cena=ilhamar, x=300, y=190, w=50)
+boia8 = Elemento(BOIA, cena=ilhamar, x=450, y=230, w=50)
+boia9 = Elemento(BOIA, cena=ilhamar, x=600, y=270, w=50)
+boia10 = Elemento(BOIA, cena=ilhamar, x=750, y=300, w=50)
