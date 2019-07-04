c46355a9f66857ba89062467b837f4e93ef64c6c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-07-04 12:00:36 -0300

    Saved cobra/__log__.py at Timestamp: 2019-07-04 15:00:36

--- cobra/__log__.py
+++ cobra/__log__.py
@@ -141,3 +141,23 @@ NameError: name 'a_floresta' is not defined
     self.floresta_inicio = Cena(FLORESTA, direita=floresta_faca)
 NameError: name 'floresta_faca' is not defined
 '''},
+{'date': 'Thu Jul 04 2019 12:00:35.407 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 118
+    a_floresta = FlorestaBanana()
+  module <module> line 111
+    cobra = Cobra(self.floresta_inicio)
+  module <module> line 86
+    if "antidoto" in INVENTARIO.item:
+AttributeError: 'Inventario' object has no attribute 'item'
+'''},
