d7da183a2ce6a8afc5b79752856eea60eddfc4ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-07-04 10:22:11 -0300

    Saved morgan/__log__.py at Timestamp: 2019-07-04 13:22:11

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -199,3 +199,23 @@ NameError: name 'LEAO_COME' is not defined
     self.falou = Texto(self.floresta_inicio, LEAO_USA)
 NameError: name 'LEAO_USA' is not defined
 '''},
+{'date': 'Thu Jul 04 2019 10:22:52.880 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 75
+    a_floresta = FlorestaLeao()
+  module <module> line 65
+    leao = Leao(self.floresta_inicio)
+  module <module> line 38
+    self.mordida = Elem(MORDIDA, x=0, y=0, w=1400, h=650, style={"opacity": 0.5}, tipo="100% 100%") #, vai=self.antidoto)
+NameError: name 'Elem' is not defined
+'''},
