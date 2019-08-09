c433c863c408a4f97699d15a4d1e1ec6bf9ca461
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-09 14:11:47 -0300

    Saved julia/__score__.py at Timestamp: 2019-08-09 17:11:47

--- julia/__score__.py
+++ julia/__score__.py
@@ -3,3 +3,4 @@
 {'doc_id': '2dd768da', 'carta': 'Nickolas Vieira', 'casa': 'pescaria', 'move': 'JULIA', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1565370511667},
   {'doc_id': '2dd768da', 'carta': 'peixe grande  ', 'casa': [453, 509], 'move': 'OLHA', 'ponto': True, 'valor': 0, '_level': 1, 'tempo': 1565370621506},
   {'doc_id': '2dd768da', 'carta': ' peixe grande ', 'casa': [573, 548], 'move': 'OLHA', 'ponto': True, 'valor': 0, '_level': 1, 'tempo': 1565370649514},
+  {'doc_id': '2dd768da', 'carta': ' peixe médio', 'casa': [374, 617], 'move': 'OLHA', 'ponto': True, 'valor': 0, '_level': 1, 'tempo': 1565370700279},
