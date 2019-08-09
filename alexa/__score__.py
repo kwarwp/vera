114648d72e49821940d25d187041ef9f10f0fd93
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-08 23:52:10 -0300

    Saved alexa/__score__.py at Timestamp: 2019-08-09 02:52:09

--- alexa/__score__.py
+++ alexa/__score__.py
@@ -5,3 +5,4 @@
   {'doc_id': '276b89cd', 'carta': 'e', 'casa': [508, 445], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'GENTE', '_level': 1, 'tempo': 1565318957547},
   {'doc_id': '276b89cd', 'carta': 'c', 'casa': [489, 211], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'COISA', '_level': 1, 'tempo': 1565318961046},
 {'doc_id': '276b89cd', 'carta': 'Outro Teste', 'casa': [], 'move': 'ID', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1565319126251},
+  {'doc_id': '276b89cd', 'carta': 'tenis', 'casa': [800, 231], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'COISA', '_level': 1, 'tempo': 1565319129478},
