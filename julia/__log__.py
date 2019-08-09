6cb157ece76c62bcb00baec296c29a08ad4dedcc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-09 11:20:12 -0300

    Saved julia/__log__.py at Timestamp: 2019-08-09 14:20:11

--- julia/__log__.py
+++ julia/__log__.py
@@ -687,3 +687,25 @@ NameError: name 'PEIXEDOZE' is not defined
     class jogoPescaria(cena):
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Fri Aug 09 2019 11:20:10.793 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 193
+    jogoPescaria()
+  module <module> line 155
+    Peixe(pescaria, x=550, y=505)
+  module <module> line 35
+    pescaria.cadastra(peixe.tit, peixe)
+  module <module> line 183
+    self.peixe[tipo][nome] = peixe
+NameError: name 'tipo' is not defined
+'''},
