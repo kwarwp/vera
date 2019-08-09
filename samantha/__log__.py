daf113087dd617a580f7f2eef68e3643eefd9bea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-09 10:07:45 -0300

    Saved samantha/__log__.py at Timestamp: 2019-08-09 13:07:45

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -386,3 +386,21 @@ AttributeError: 'Elemento' object has no attribute 'entr'
     letra_s.entr(parque)
 AttributeError: 'Elemento' object has no attribute 'entr'
 '''},
+{'date': 'Fri Aug 09 2019 10:07:44.857 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 107
+    JogoLetra()
+  module <module> line 52
+    letra_a = Elemento(LETRA_A, x=540, y=470, w=90, h=20, drag=True)
+TypeError: 'module' object is not callable
+'''},
