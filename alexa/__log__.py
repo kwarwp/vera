5dd88ee58403e363cf2c3b8430b5472dbfccdbaa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-08 21:18:50 -0300

    Saved alexa/__log__.py at Timestamp: 2019-08-09 00:18:50

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -438,3 +438,25 @@ SyntaxError: invalid syntax
                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Aug 08 2019 21:18:49.274 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 79
+    Conjuntos()
+  module <module> line 62
+    Fruta(parque, x=30, y=90)
+  module <module> line 22
+    parque.cadastra(tit, maca)
+  module <module> line 73
+    self.coisas[tit] = elm
+AttributeError: 'Conjuntos' object has no attribute 'coisas'
+'''},
