4fa9eee13ccc51668bae917a1bb1a552f5b49680
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-08 23:55:58 -0300

    Saved alexa/main.py at Timestamp: 2019-08-09 02:55:57

--- alexa/main.py
+++ alexa/main.py
@@ -1,5 +1,5 @@
 # vera.alexa.main.py
-from _spy.vitollino.main import Cena,STYLE,Codigo, INVENTARIO
+from _spy.vitollino.main import Cena,STYLE,Codigo, INVENTARIO, Texto
 from elemento.main import Elemento 
 STYLE["width"] = 600
 STYLE["heigth"] = "600px"
@@ -15,25 +15,27 @@ CRIANCA = "https://i.imgur.com/j0ETf5x.jpg"
 TENIS = "https://i.imgur.com/nwKWo8x.png"
 GALOCHA = "https://i.imgur.com/JlGV4P8.png"
 ALL = "coelho passarinho bola peteca maçã laranja tenis galocha".split()
-def score(casa=[], carta="", move="DROP", ponto="OK", valor="", _level=1):
-        INVENTARIO.elt.html = f"casa={[ev.x, ev.y]}, carta={tit}, move=DROP, ponto={ok}, valor={cria[9]}"
+def score(casa=[], carta="", move="ID", ponto="OK", valor="", _level=1):
+    #INVENTARIO.elt.html = f"casa={casa}, carta={carta}, move={move}, ponto={ponto}, valor={valor}, level={_level}"
+    INVENTARIO.score(casa=casa, carta=carta, move=move, ponto=ponto, valor=valor, _level=_level)
 
 
 class Coisa:
     def __init__(self):
         pass
     def click(self, ev):
-        score(casa=[ev.x, ev.y], carta=self.tit[0], move="CLICK", ponto="OK", valor="COISA", _level=1)
+        Texto(self.parque, self.tit).vai()
+        score(casa=[ev.x, ev.y], carta=self.tit, move="CLICK", ponto="OK", valor="COISA", _level=1)
 
 class Fruta(Coisa):
     def __init__(self, parque,tit="maçã", imagem=MACA, x=200, y=200):
-        self.tit = tit
+        self.parque, self.tit = parque, tit
         maca = Elemento(imagem, tit=tit, x=x, y=y, w=50, h=40, drag=True, tipo="100% 100%", vai=self.click)
         maca.entra(parque)
         parque.cadastra(tit, maca)
 class Esportes(Coisa):  
     def __init__(self, parque,tit="bola", imagem=BOLA, x=100, y=200): 
-        self.tit = tit
+        self.parque, self.tit = parque, tit
         peteca = Elemento(imagem, tit=tit, x=x, y=y, w=50, h=50, drag=True, vai=self.click)
         peteca.entra(parque)
         parque.cadastra(tit, peteca)
@@ -44,7 +46,7 @@ class Bicho(Coisa):
     def __init__(self, parque, tit="coelho", imagem=COELHO, x=40, y=40):
         #coelho = Elemento(COELHO, x=100, y=100, w=60, h=40)
         #coelho.entra(parque)
-        self.tit = tit
+        self.parque, self.tit = parque, tit
         passarinho = Elemento(imagem, tit=tit, x=x, y=y, w=70, h=50, drag=True, vai=self.click)
         passarinho.entra(parque)
         parque.cadastra(tit, passarinho)
@@ -53,19 +55,20 @@ class Calcado(Coisa):
     def __init__(self,parque, tit="tenis", imagem=TENIS, x=300, y=350):
         #tenis = Elemento(TENIS, x=150, y=210, w=50, h=80)
         #tenis.entra(parque)
-        self.tit = tit
+        self.parque, self.tit = parque, tit
         galocha = Elemento(imagem, tit=tit, x=x, y=y, w=60, h=50, drag=True, vai=self.click)
         galocha.entra(parque)
         parque.cadastra(tit, galocha)
         
 class Crianca:
     def __init__(self, parque, tit="crianca", x=0, y=210, gosta=""):
-        self.tit = tit
-        dragger = {ti: lambda e,t: parque.gostou(e, t, tit, ok=ti in gosta.split()) for ti in ALL}
-        crianca = Elemento(CRIANCA, tit=tit, x=x-10, y=y-10, w=90, h=140, style={"opacity": 0.3}, drop=dragger)
+        self.parque, self.tit = parque, tit
+        dragger = {ti: lambda e,t,i=ti in gosta: parque.gostou(e, t, tit, ok=i) for ti in ALL}
+        crianca = Elemento(CRIANCA, tit=tit, x=x-10, y=y-10, w=90, h=140, style={"opacity": 0.3}, drop=dragger, vai=self.click)
         crianca.entra(parque)
         
     def click(self, ev):
+        Texto(self.parque, f"Eu {self.tit}").vai()
         score(casa=[ev.x, ev.y], carta=self.tit[9], move="CLICK", ponto="OK", valor="GENTE", _level=1)
         
 class Conjuntos(Cena):
@@ -74,11 +77,14 @@ class Conjuntos(Cena):
         self.coisas = {}
         parque = self  # Cena(CENA_PARQUE)
         INVENTARIO.inicia()
+        participante = input("diga seu nome")
+        score(carta=participante, _level=0)
         #INVENTARIO.elt.html = "PARQUE"
         nome = Codigo(codigo="", topo="PARQUE", style=dict(left=250, top=220, width=100, height="60px"))
         nome.entra(parque)
         Crianca(parque, tit="gosto de bicho", x=60, y=300, gosta="coelho passarinho")
         Crianca(parque, tit="gosto de esportes", x=150, y=300, gosta="bola peteca")
+        Crianca(parque, tit="gosto de passear", x=270, y=300, gosta="") 
         Crianca(parque, tit="gosto de frutas", x=400, y=300, gosta="maçã laranja") 
         Crianca(parque, tit="gosto de calçado", x=480, y=300, gosta="tenis galocha")
         Fruta(parque, x=30, y=90)
@@ -95,11 +101,12 @@ class Conjuntos(Cena):
         self.coisas[tit] = elm
         
     def gostou(self, ev, tit, cria, ok):
+        score(casa=[ev.x, ev.y], carta=tit, move="DROP", ponto=ok, valor=cria[9], _level=1)
         #INVENTARIO.elt.html = tit
         #INVENTARIO.score(casa=[ev.x, ev.y], carta=[tit], move="DROP", ponto="OK", valor=cria[9], _level=1):
         #INVENTARIO.elt.html = dict(casa=[ev.x, ev.y], carta=tit, move="DROP", ponto=ok, valor=cria[9], _level=1)
-        INVENTARIO.elt.html = f"casa={[ev.x, ev.y]}, carta={tit}, move=DROP, ponto={ok}, valor={cria[9]}"
-        #INVENTARIO.bota(self.coisas[tit]) if ok else None
+        #INVENTARIO.elt.html = f"casa={[ev.x, ev.y]}, carta={tit}, move=DROP, ponto={ok}, valor={cria[9]}"
+        INVENTARIO.bota(self.coisas[tit]) if ok else None
         
         
 Conjuntos()
