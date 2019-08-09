b6ce38a44c4593f4b09a19b74250a5334091b824
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-09 08:56:11 -0300

    Saved alexa/__score__.py at Timestamp: 2019-08-09 11:56:11

--- alexa/__score__.py
+++ alexa/__score__.py
@@ -1,3 +1,4 @@
 
 {'doc_id': '9b328050', 'carta': 'Caua Teixeira de Oliveira Sousa', 'casa': [], 'move': 'ID', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1565351725441},
 {'doc_id': '9b328050', 'carta': 'Caua', 'casa': [], 'move': 'ID', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1565351747113},
+  {'doc_id': '9b328050', 'carta': 'f', 'casa': [446, 428], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'GENTE', '_level': 1, 'tempo': 1565351769627},
