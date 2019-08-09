8463bbf37eef725e410ce9830cffa4a4c0da00c5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-08 23:55:28 -0300

    Clear test 0

--- alexa/__score__.py
+++ alexa/__score__.py
@@ -1,12 +1,1 @@
-  {'doc_id': '276b89cd', 'carta': 'p', 'casa': [631, 439], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'GENTE', '_level': 1, 'tempo': 1565318939154},
-  {'doc_id': '276b89cd', 'carta': 'passarinho', 'casa': [741, 445], 'move': 'DROP', 'ponto': False, 'valor': 'f', '_level': 1, 'tempo': 1565318942648},
-  {'doc_id': '276b89cd', 'carta': 'peteca', 'casa': [828, 451], 'move': 'DROP', 'ponto': False, 'valor': 'c', '_level': 1, 'tempo': 1565318950679},
-  {'doc_id': '276b89cd', 'carta': 'peteca', 'casa': [508, 445], 'move': 'DROP', 'ponto': True, 'valor': 'e', '_level': 1, 'tempo': 1565318954697},
-  {'doc_id': '276b89cd', 'carta': 'e', 'casa': [508, 445], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'GENTE', '_level': 1, 'tempo': 1565318957547},
-  {'doc_id': '276b89cd', 'carta': 'c', 'casa': [489, 211], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'COISA', '_level': 1, 'tempo': 1565318961046},
-{'doc_id': '276b89cd', 'carta': 'Outro Teste', 'casa': [], 'move': 'ID', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1565319126251},
-  {'doc_id': '276b89cd', 'carta': 'tenis', 'casa': [800, 231], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'COISA', '_level': 1, 'tempo': 1565319129478},
-  {'doc_id': '276b89cd', 'carta': 'coelho', 'casa': [493, 209], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'COISA', '_level': 1, 'tempo': 1565319133522},
-  {'doc_id': '276b89cd', 'carta': 'bola', 'casa': [499, 448], 'move': 'DROP', 'ponto': True, 'valor': 'e', '_level': 1, 'tempo': 1565319143591},
-  {'doc_id': '276b89cd', 'carta': 'laranja', 'casa': [749, 435], 'move': 'DROP', 'ponto': True, 'valor': 'f', '_level': 1, 'tempo': 1565319150783},
-  {'doc_id': '276b89cd', 'carta': 'tenis', 'casa': [628, 449], 'move': 'DROP', 'ponto': False, 'valor': 'p', '_level': 1, 'tempo': 1565319155398},
+
