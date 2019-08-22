0da84267068da1b1ed3c7d349c21daaac58cb303
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 10:53:25 -0300

    Saved kristen/main.py at Timestamp: 2019-08-22 13:53:25

--- kristen/main.py
+++ kristen/main.py
@@ -2,11 +2,13 @@
 from _spy.vitollino.main import Cena
 TEMPLO ="https://i.imgur.com/dQqwYul.jpg"
 ACAMPAMENTO = "https://i.imgur.com/ba43tjx.jpg"
+MOCHILA = "https://i.imgur.com/sTWdwDV.png"
 TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
 FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
 ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
 COBRA = "https://i.imgur.com/MydpgBT.jpg"
 DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
+MUMIA = "https://i.imgur.com/3215w01.jpg" 
 templo = Cena(TEMPLO)
 templo.vai()
 acampamento = Cena(ACAMPAMENTO, esquerda=templo)
@@ -30,4 +32,13 @@ tesouro.vai()
 desabamento = Cena(DESABAMENTO, esquerda=tesouro)
 tesouro.direita = desabamento
 desabamento.vai()
+tesouro = Cena(TESOURO, esquerda=desabamento)
+desabamento.direita = tesouro
+tesouro.vai()
+tesouro = Cena(TESOURO, esquerda=tesouro)
+tesouro.direita = tesouro
+terouro.vai()
+mumia = Cena(MUMIA, esquerda=tesouro)
+tesouro.direita = mumia
+mumia.vai()
 
