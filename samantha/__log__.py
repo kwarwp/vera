e94bbdbf8da062374f6712c07d32ea9dbdf7f0f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 09:31:47 -0300

    Saved samantha/__log__.py at Timestamp: 2019-08-22 12:31:46

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -499,3 +499,10 @@ AttributeError: 'JogoLetra' object has no attribute 'letra_b'
                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Aug 22 2019 09:31:46.436 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 66
+  self.letra_g = Elemento(LETRA_G,tit="G" x=450, y=255, w=10, h=30, drag=True,vai=self.clicounaletra)
+                                           ^
+SyntaxError: invalid syntax
+'''},
