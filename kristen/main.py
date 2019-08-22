0f757c10eacafc3cc75bc31c579f054e722cdbf2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 10:22:44 -0300

    Saved kristen/main.py at Timestamp: 2019-08-22 13:22:44

--- kristen/main.py
+++ kristen/main.py
@@ -5,6 +5,8 @@ ACAMPAMENTO = "https://i.imgur.com/ba43tjx.jpg"
 TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
 FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
 ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
+COBRA = "https://i.imgur.com/MydpgBT.jpg"
+DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
 templo = Cena(TEMPLO)
 templo.vai()
 acampamento = Cena(ACAMPAMENTO, esquerda=templo)
@@ -19,4 +21,12 @@ fogo.vai()
 artefato = Cena(ARTEFATO1, esquerda=fogo)
 fogo.direita = artefato
 artefato.vai()
-
+cobra = Cena(COBRA, esquerda=artefato)
+artefato.direita = cobra
+cobra.vai()
+tesouro = Cena(TESOURO, esquerda=cobra)
+cobra.direita = tesouro
+tesouro,vai()
+desabamento = Cena(DESABAMENTO, esquerda=tesouro)
+tesouro.direita = desabamento
+desabamento.vai()
