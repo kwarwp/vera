ab736a4c37abc411f40b9d2ce7ceab32714822b9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 09:58:48 -0300

    Saved kristen/main.py at Timestamp: 2019-08-22 12:58:48

--- kristen/main.py
+++ kristen/main.py
@@ -6,7 +6,12 @@ TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
 FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
 templo = Cena(TEMPLO)
 templo.vai()
-acampamento = Cena(ACAMPAMENTO, direita=templo)
-templo.esquerda = acampamento
+acampamento = Cena(ACAMPAMENTO, esquerda=templo)
+templo.direita = acampamento
 acampamento.vai()
-
+tesouro = Cena(TESOURO, esquerda=acampamento)
+acampamento.direita = tesouro
+tesouro.vai()
+fogo = Cena(FOGO, esquerda=tesouro)
+tesouro.direita = fogo
+fogo.vai()
