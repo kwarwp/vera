b342a7d58d26c7209e2d0a167c17b0687e9949fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 09:40:39 -0300

    Saved samantha/__log__.py at Timestamp: 2019-08-22 12:40:38

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -506,3 +506,21 @@ SyntaxError: invalid syntax
                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Aug 22 2019 09:40:38.323 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 114
+    JogoLetra()
+  module <module> line 108
+    self.mapa=dict(G=self.letra_g,H=self.letra_h,I=self.letra_i,J=self.letra_j,K=self.letra_k,L=self.letra_l)
+AttributeError: 'JogoLetra' object has no attribute 'letra_h'
+'''},
