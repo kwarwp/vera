70854e7578d7fdebf38bcd2e8c6419b695a0a39a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 11:00:12 -0300

    Saved kristen/__log__.py at Timestamp: 2019-08-22 14:00:11

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -171,3 +171,19 @@ NameError: name 'terouro' is not defined
     terouro.vai()
 NameError: name 'terouro' is not defined
 '''},
+{'date': 'Thu Aug 22 2019 11:00:10.951 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    terouro2.vai()
+NameError: name 'terouro2' is not defined
+'''},
