59f95379286e2db3efa7052938a71dbb2d65a068
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 09:31:12 -0300

    Saved kristen/main.py at Timestamp: 2019-08-22 12:31:11

--- kristen/main.py
+++ kristen/main.py
@@ -1,5 +1,8 @@
 # vera.kristen.main.py
 from _spy.vitollino.main import Cena
 TEMPLO ="https://i.imgur.com/dQqwYul.jpg"
+ACAMPAMENTO = "https://i.imgur.com/ba43tjx.jpg"
 templo = Cena(TEMPLO)
 templo.vai()
+acampamento = cena(ACAMPAMENTO)
+acampamento.vai()
