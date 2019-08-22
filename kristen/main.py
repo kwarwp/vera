3e33daf54410ddb37a5c01913cc343a47fd13910
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-22 09:52:36 -0300

    Saved kristen/main.py at Timestamp: 2019-08-22 12:52:35

--- kristen/main.py
+++ kristen/main.py
@@ -2,8 +2,11 @@
 from _spy.vitollino.main import Cena
 TEMPLO ="https://i.imgur.com/dQqwYul.jpg"
 ACAMPAMENTO = "https://i.imgur.com/ba43tjx.jpg"
+TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
+FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
 templo = Cena(TEMPLO)
 templo.vai()
 acampamento = Cena(ACAMPAMENTO, direita=templo)
 templo.esquerda = acampamento
 acampamento.vai()
+
