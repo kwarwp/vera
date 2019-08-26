5ac7910c1f8202c5505f75f0639154d862cf8816
Carlo E. T. Oliveira <carlo@nce.ufrj.br>
2019-08-26 14:10:32 -0300

    name vera, victoria

---
+++ __author__.py
@@ -0,0 +1,3 @@
+{
+  "vera": {"author_nick": "fonocomp", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
+  "kristen": {"author_nick": "victoria", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
