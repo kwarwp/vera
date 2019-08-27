ac22ee462b1f167488cd00f12e73aefbd65874f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 19:46:49 -0300

    Saved kristen/__log__.py at Timestamp: 2019-08-27 22:46:48

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -590,3 +590,19 @@ AttributeError: 'Inca' object has no attribute '__len__'
     shuffle[Inca()]
 AttributeError: 'function' object has no attribute '__getitem__'
 '''},
+{'date': 'Tue Aug 27 2019 19:46:48.867 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    shuffle[Inca()]
+AttributeError: 'function' object has no attribute '__getitem__'
+'''},
