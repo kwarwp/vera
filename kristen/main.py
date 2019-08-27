a54673cf6af789f287a4745622be8d95299dbc32
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 20:40:32 -0300

    Saved kristen/main.py at Timestamp: 2019-08-27 23:40:31

--- kristen/main.py
+++ kristen/main.py
@@ -1,9 +1,7 @@
 # vera.kristen.main.py
 from _spy.vitollino.main import Cena
 from random import shuffle
-TEMPLO ="https://i.imgur.com/dQqwYul.jpg"
-ACAMPAMENTO = "https://i.imgur.com/ba43tjx.jpg"
-MOCHILA = "https://i.imgur.com/sTWdwDV.png"
+TEMPLO = "https://i.imgur.com/dQqwYul.jpg"
 TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
 FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
 ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
@@ -11,43 +9,38 @@ COBRA = "https://i.imgur.com/MydpgBT.jpg"
 DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
 MUMIA = "https://i.imgur.com/3215w01.jpg" 
 ARANHA = "http://varg.wdfiles.com/local--files/sr-annals-1/Spiders.jpg"
+
+templo = Cena(TEMPLO)
+templo.vai()
+tesouro = Cena(TESOURO)
+tesouro.vai()
+fogo = Cena(FOGO)
+fogo.vai()
+artefato1 = Cena(ARTEFATO1)
+artefato1.vai()
+cobra = Cena(COBRA)
+cobra.vai()
+desabamento = Cena(DESABAMENTO)
+desabamento.vai()
+mumia = Cena(MUMIA)
+mumia.vai()
+aranha = Cena(ARANHA)
+aranha.vai()
+
 def decisao (anterior,carta):
     resposta = input ("Você descobriu uma câmara do templo. Você entra nela ou sai?")
     if resposta == "entro": 
         carta.vai() 
     elif resposta == "saio":
         anterior.vai()
-RODADA = "Primeira Rodada"
-templo = Cena(TEMPLO)
-acampamento = Cena(ACAMPAMENTO, esquerda=templo)
-templo.direita = acampamento
-tesouro = Cena(TESOURO, esquerda=acampamento)
-acampamento.direita = tesouro
-fogo = Cena(FOGO, esquerda=tesouro)
+
+cartas = ['templo', 'tesouro', 'fogo', 'artefato1', 'cobra', 'desabamento', 'mumia', 'aranha']
+baralho = cartas[:] # Copy cartas
+shuffle(baralho) # Shuffle baralho
+baralho.vai()
+
 # decisao(tesouro, fogo)
-tesouro.direita = fogo
-artefato = Cena(ARTEFATO1, esquerda=fogo)
-fogo.direita = artefato
-cobra = Cena(COBRA, esquerda=artefato)
-artefato.direita = cobra
-tesouro0 = Cena(TESOURO, esquerda=cobra)
-cobra.direita = tesouro0
-desabamento = Cena(DESABAMENTO, esquerda=tesouro0)
-tesouro0.direita = desabamento
-tesouro1 = Cena(TESOURO, esquerda=desabamento)
-desabamento.direita = tesouro1
-tesouro2 = Cena(TESOURO, esquerda=tesouro1)
-tesouro1.direita = tesouro2
-mumia = Cena(MUMIA, esquerda=tesouro2)
-tesouro2.direita = mumia
-tesouro3 = Cena(TESOURO, esquerda=mumia)
-mumia.direita = tesouro3
-aranha = Cena(ARANHA, esquerda=tesouro3)
-tesouro3.direita = aranha
-fogo2 = Cena(FOGO, esquerda=aranha)
-aranha.direita = fogo2
-baralho = [fogo, tesouro, aranha, mumia, artefato, desabamento]
-shuffle(baralho)
-umacarta = baralho[0]
-umacarta.vai()
+
+
+
 
