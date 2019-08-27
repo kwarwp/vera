3b9353f0338b60bcd7404e2019179233d819302b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 20:35:46 -0300

    Saved kristen/__log__.py at Timestamp: 2019-08-27 23:35:46

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -717,3 +717,19 @@ AttributeError: 'list' object has no attribute 'vai'
     baralho.vai() # Possible Output: ['templo', 'tesouro', 'fogo', 'artefato1', 'cobra', 'desabamento', 'mumia', 'aranha']
 AttributeError: 'list' object has no attribute 'vai'
 '''},
+{'date': 'Tue Aug 27 2019 20:35:46.500 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    baralho.vai() 
+AttributeError: 'list' object has no attribute 'vai'
+'''},
