8934cae76d261cda80188253c85c2d7cade645da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 21:25:34 -0300

    Saved kristen/main.py at Timestamp: 2019-08-28 00:25:33

--- kristen/main.py
+++ kristen/main.py
@@ -1,6 +1,9 @@
 # vera.kristen.main.py
 from _spy.vitollino.main import Cena
+from random import shuffle
 TEMPLO = "https://i.imgur.com/dQqwYul.jpg"
+ACAMPAMENTO = "https://i.imgur.com/ba43tjx.jpg"
+MOCHILA = "https://i.imgur.com/sTWdwDV.png"
 TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
 FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
 ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
@@ -14,24 +17,39 @@ def decisao (anterior,carta):
         carta.vai() 
     elif resposta == "saio":
         anterior.vai()
+RODADA = "Primeira Rodada"
 templo = Cena(TEMPLO)
-tesouro = Cena(TESOURO, esquerda=templo)
-templo.direita = tesouro
+acampamento = Cena(ACAMPAMENTO, esquerda=templo)
+templo.direita = acampamento
+tesouro = Cena(TESOURO, esquerda=acampamento)
+acampamento.direita = tesouro
 fogo = Cena(FOGO, esquerda=tesouro)
-tesouro.direita = fogo
-artefato1 = Cena(ARTEFATO1, esquerda=fogo)
-fogo.direita = artefato1
-cobra = Cena(COBRA, esquerda=artefato1)
-artefato1.direita = cobra
-desabamento = Cena(DESABAMENTO, esquerda=cobra)
-cobra.direita = desabamento
-mumia = Cena(MUMIA, esquerda=desabamento)
-desabamento.direita = mumia
-
-
-
 # decisao(tesouro, fogo)
-
+tesouro.direita = fogo
+artefato = Cena(ARTEFATO1, esquerda=fogo)
+fogo.direita = artefato
+cobra = Cena(COBRA, esquerda=artefato)
+artefato.direita = cobra
+tesouro0 = Cena(TESOURO, esquerda=cobra)
+cobra.direita = tesouro0
+desabamento = Cena(DESABAMENTO, esquerda=tesouro0)
+tesouro0.direita = desabamento
+tesouro1 = Cena(TESOURO, esquerda=desabamento)
+desabamento.direita = tesouro1
+tesouro2 = Cena(TESOURO, esquerda=tesouro1)
+tesouro1.direita = tesouro2
+mumia = Cena(MUMIA, esquerda=tesouro2)
+tesouro2.direita = mumia
+tesouro3 = Cena(TESOURO, esquerda=mumia)
+mumia.direita = tesouro3
+aranha = Cena(ARANHA, esquerda=tesouro3)
+tesouro3.direita = aranha
+fogo2 = Cena(FOGO, esquerda=aranha)
+aranha.direita = fogo2
+baralho = [fogo, tesouro, aranha, mumia, artefato, desabamento]
+shuffle(baralho)
+umacarta = baralho[0]
+umacarta.vai()
 
 
 
