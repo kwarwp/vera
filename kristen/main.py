43bcf064b5cbb1473e2924322524e797008e65b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 21:07:41 -0300

    Saved kristen/main.py at Timestamp: 2019-08-28 00:07:41

--- kristen/main.py
+++ kristen/main.py
@@ -1,6 +1,5 @@
 # vera.kristen.main.py
 from _spy.vitollino.main import Cena
-from random import shuffle
 TEMPLO = "https://i.imgur.com/dQqwYul.jpg"
 TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
 FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
@@ -8,36 +7,28 @@ ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
 COBRA = "https://i.imgur.com/MydpgBT.jpg"
 DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
 MUMIA = "https://i.imgur.com/3215w01.jpg" 
-ARANHA = "http://varg.wdfiles.com/local--files/sr-annals-1/Spiders.jpg"
-
-templo = Cena(TEMPLO)
-templo.vai()
-tesouro = Cena(TESOURO)
-tesouro.vai()
-fogo = Cena(FOGO)
-fogo.vai()
-artefato1 = Cena(ARTEFATO1)
-artefato1.vai()
-cobra = Cena(COBRA)
-cobra.vai()
-desabamento = Cena(DESABAMENTO)
-desabamento.vai()
-mumia = Cena(MUMIA)
-mumia.vai()
-aranha = Cena(ARANHA)
-aranha.vai()
-
+ARANHA = "https://i.imgur.com/JjEFJg0.jpg"
 def decisao (anterior,carta):
     resposta = input ("Você descobriu uma câmara do templo. Você entra nela ou sai?")
     if resposta == "entro": 
         carta.vai() 
     elif resposta == "saio":
         anterior.vai()
+templo = Cena(TEMPLO)
+tesouro = Cena(TESOURO, esquerda=templo)
+templo.direita = tesouro
+fogo = Cena(FOGO, esquerda=tesouro)
+tesouro.direita = fogo
+artefato1 = Cena(ARTEFATO1, esquerda=fogo)
+fogo.direita = artefato1
+cobra = Cena(COBRA, esquerda=artefato1)
+artefato1.direita = cobra
+desabamento = Cena(DESABAMENTO, esquerda=cobra)
+cobra.direita = desabamento
+mumia = Cena(MUMIA, esquerda=desabamento)
+desabamento.direita = mumia
 
-cartas = ['templo', 'tesouro', 'fogo', 'artefato1', 'cobra', 'desabamento', 'mumia', 'aranha']
-baralho = cartas[:] # Copy cartas
-shuffle(baralho) # Shuffle baralho
-baralho.vai()
+
 
 # decisao(tesouro, fogo)
 
