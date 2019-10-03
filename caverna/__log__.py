e937bea33b7e405590fef87e12ac78e925947cc6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-03 11:03:34 -0300

    Saved caverna/__log__.py @2019-10-03 14:03

--- caverna/__log__.py
+++ caverna/__log__.py
@@ -24,3 +24,21 @@ NameError: name 'Caverna' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Oct 03 2019 11:03:33.580 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 70
+    a_floresta = Caverna()
+  module <module> line 58
+    self.entrada_da_caverna = Elemento(A_ENTRADA, tit="a entrada da caverna escura", style=dict(
+NameError: name 'A_ENTRADA' is not defined
+'''},
