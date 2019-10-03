44738f331c62d4198134ca8c586212667aa2b68e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-03 12:47:18 -0300

    Saved caverna/__log__.py @2019-10-03 15:47

--- caverna/__log__.py
+++ caverna/__log__.py
@@ -145,3 +145,23 @@ NameError: name 'Elemrnto' is not defined
     self.elt <= item_img
 UnboundLocalError: local variable 'item_img' referenced before assignment
 '''},
+{'date': 'Thu Oct 03 2019 12:47:16.978 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 83
+    a_floresta = Caverna()
+  module <module> line 70
+    INVENTARIO.bota("lanterna","https://i.imgur.com/Z4DAh02.png",drag=True)
+  module _spy.vitollino.main line 359
+    self.elt <= item_img
+UnboundLocalError: local variable 'item_img' referenced before assignment
+'''},
