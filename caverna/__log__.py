7e7018a71c56b2658526b399ca46fd5e878638d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-03 11:31:37 -0300

    Saved caverna/__log__.py @2019-10-03 14:31

--- caverna/__log__.py
+++ caverna/__log__.py
@@ -62,3 +62,21 @@ NameError: name 'A_ENTRADA' is not defined
     def __init__(self, img="", vai=None, style=NDCT, tit="", alt="",
 NameError: name 'NDCT' is not defined
 '''},
+{'date': 'Thu Oct 03 2019 11:31:36.739 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from _spy.vitollino.main import Cena, Texto, INVENTARIO, STYLE
+  module _spy.vitollino.main line 486
+    class Elemento(Element_):
+NameError: name 'Element_' is not defined
+'''},
