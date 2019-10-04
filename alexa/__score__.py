0fc7d24cd4f7c5ea5352ef6cf660fb866236bda4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-04 08:21:35 -0300

    Saved alexa/__score__.py @2019-10-04 11:21

--- alexa/__score__.py
+++ alexa/__score__.py
@@ -245,3 +245,4 @@
 {'doc_id': '74270114', 'carta': 'julia almeida couto', 'casa': [], 'move': 'ID', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1570188004237},
   {'doc_id': '74270114', 'carta': 'f', 'casa': [530, 461], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'GENTE', '_level': 1, 'tempo': 1570188052742},
   {'doc_id': '74270114', 'carta': 'b', 'casa': [214, 464], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'GENTE', '_level': 1, 'tempo': 1570188091182},
+  {'doc_id': '74270114', 'carta': 'b', 'casa': [201, 419], 'move': 'CLICK', 'ponto': 'OK', 'valor': 'GENTE', '_level': 1, 'tempo': 1570188095365},
