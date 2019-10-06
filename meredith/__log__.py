bdea7aeac592db1033015ccf3008005a5edb4a0f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 20:00:43 -0300

    Saved meredith/__log__.py @2019-10-06 23:00

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -62,3 +62,46 @@ Traceback (most recent call last):
     self.perigo = Perigo()
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Sun Oct 06 2019 19:59:24.810 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from lisa.main import Partida
+  module lisa.main line 2
+    from kathryn.main import Rodada 
+  module kathryn.main line 5
+    from natalia.main import Jogador
+  module natalia.main line 2
+    from kathryn.main import Rodada
+ImportError: cannot import name 'Rodada'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from lisa.main import Partida
+  module lisa.main line 2
+    from kathryn.main import Rodada 
+  module kathryn.main line 5
+    from natalia.main import Jogador
+  module natalia.main line 2
+    from kathryn.main import Rodada
+'''},
