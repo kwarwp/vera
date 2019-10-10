fbf0de3bcc6c75c02f793c13a30e61ad91bd0925
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 11:50:41 -0300

    Created final/__log__.py @2019-10-10 14:50

---
+++ final/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Thu Oct 10 2019 11:50:38.543 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    a_floresta = FlorestaFinal()
+  module <module> line 56
+    self.macaca_mae = Macaca()
+TypeError: __init__() missing 1 positional argument: aqui
+'''},
