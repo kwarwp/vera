dda9e7bd2a1b359b97ecf7476112c43a63ae7716
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 11:11:02 -0300

    Created final/main.py @2019-10-10 14:11

---
+++ final/main.py
@@ -0,0 +1,69 @@
+# vera.final.main.py
+"http://supygirls.pythonanywhere.com"
+from _spy.vitollino.main import Cena, Texto, INVENTARIO, STYLE
+from elemento.main import Elemento
+from cobra.main import Cobra
+CHUVA = "https://i.imgur.com/ubkw6wx.jpg"
+STYLE["width"] = 1400
+STYLE["height"] = "650px"
+# from amanda.main import FlorestaFaca
+FLORESTA = "https://i.imgur.com/VHaolvA.jpg"
+BANANA = "https://i.imgur.com/HnIHJd7.png"
+TEXTO_BANANA= "O macaquinho pode ficar com fome! Coloque na bolsa!"
+BANANA_FOI= "Hummm, que delícia!"
+BANANA_USA= "Você segura a banana na mão!"
+REDE = "https://i.imgur.com/9Fig2oH.png"
+TEXTO_REDE= "Caí na armadilha! Pegue a faca para cortá-la!"
+COBRA = "https://i.imgur.com/nQ0StLK.png"
+BISCOITO = "https://i.imgur.com/ywr5b2D.png"
+CAVERNA = "https://i.imgur.com/00MjS1k.jpg"
+LANTERNA = "https://i.imgur.com/OO5pLxV.png"
+INTERIOR_CAVERNA = "https://i.imgur.com/lNPYjUA.jpg"
+FLORESTA_CHUVA = "https://i.imgur.com/sDQ5r36.jpg"
+CAPA_DE_CHUVA = "https://i.imgur.com/09U7IBK.png"
+RIO = "https://i.imgur.com/uYrWcA2.jpg"
+CORDA = "https://i.imgur.com/lCWG2Co.png"
+
+class CenaProxy:
+    def __init__(self, aqui=None):
+        self.aqui = aqui
+        self.floresta_rio = None
+    def vai(self):
+        from rio.main import FlorestaRio
+        self.floresta_rio = FlorestaRio(self.aqui)
+        self.floresta_rio.esquerda = self.aqui
+        self.floresta_rio.vai()
+
+
+class Molhado:
+    def __init__(self, aqui):
+        self.aqui = aqui
+        
+    def vai(self):
+        Texto(self.aqui, "Você fica muito molhado e não consegue continuar").vai()
+
+
+class FlorestaFinal:
+    def __init__(self, esquerda=None):
+        # floresta_faca = FlorestaFaca() -XX- ERRO!
+        self.floresta_inicio = None
+        self.floresta_rio = floresta_rio = CenaProxy(self.floresta_inicio)
+        esquerda = esquerda or floresta_rio
+        self.floresta_inicio = Cena(FLORESTA_CHUVA, esquerda=esquerda)
+        self.floresta_inicio.meio = self.floresta_inicio.direita = Molhado(self.floresta_inicio) 
+        floresta_rio.aqui = self.floresta_inicio 
+        
+    def vai(self):
+        if "capa de chuva" in INVENTARIO.item:
+            INVENTARIO.item["capa de chuva"].vai = self.protege
+        self.floresta_inicio.vai()
+        
+    def protege(self, _):
+        Texto(self.floresta_inicio, "Você se protege").vai()
+        self.floresta_inicio.direita = self.floresta_rio
+        self.floresta_inicio.meio = self.floresta_rio
+               
+if __name__ == "__main__":
+    INVENTARIO.inicia()
+    a_floresta = FlorestaFinal()
+    a_floresta.vai()
