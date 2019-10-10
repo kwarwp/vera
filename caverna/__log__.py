3d5c892214728201440f8323550e9ed5c3fefa35
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 10:22:36 -0300

    Saved caverna/__log__.py @2019-10-10 13:22

--- caverna/__log__.py
+++ caverna/__log__.py
@@ -185,3 +185,21 @@ UnboundLocalError: local variable 'item_img' referenced before assignment
     self.elt <= item_elt
 TypeError: can't add 'Elemento' object to DOMNode instance
 '''},
+{'date': 'Thu Oct 10 2019 10:22:35.748 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 84
+    a_floresta = Caverna()
+  module <module> line 66
+    esquerda = esquerda or floresta_faca
+NameError: name 'floresta_faca' is not defined
+'''},
