28516ca463eabe68fbe4685a2e9108937fb3e09a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 09:53:37 -0300

    Saved soraya/main.py @2019-10-10 12:53

--- soraya/main.py
+++ soraya/main.py
@@ -1,6 +1,6 @@
 # vera.soraya.main.py
 "http://supygirls.pythonanywhere.com"
-from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE, INVENTARIO
 STYLE["width"] = 1200
 STYLE["height"] = "650px"
 FLORESTA = "https://i.imgur.com/bfO1gCN.jpg"
@@ -24,7 +24,7 @@ class FlorestaMacaco:
         floresta_inicio = Cena(FLORESTA, direita=floresta_faca)
         self.floresta_inicio = floresta_inicio
         self.fala = Texto(self.floresta_inicio, TEXTO_MACACO)
-        self.macaco = Elemento(MACACO, style=dict(left="700px", width="100px"))
+        self.macaco = Elemento(MACACO, style=dict(left="700px", top="400px", width="100px"))
         self.macaco.entra(floresta_inicio)
         self.macaco.vai=self.falamacaco
         floresta_inicio.vai()
@@ -36,5 +36,6 @@ class FlorestaMacaco:
         self.fala.vai() 
         
 if __name__ == "__main__":
+    INVENTARIO.inicia()
     a_floresta = FlorestaMacaco()
     a_floresta.vai()        
