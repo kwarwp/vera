db8f8c16bc84b7566f189dc030a11b059e025255
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 12:59:40 -0300

    Saved trilhacaverna/__log__.py @2019-10-15 15:59

--- trilhacaverna/__log__.py
+++ trilhacaverna/__log__.py
@@ -78,3 +78,34 @@ Traceback (most recent call last):
   module <module> line 5
     from cavernatesouro.main import cavernatesouro
 '''},
+{'date': 'Tue Oct 15 2019 12:59:39.465 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from cavernatesouro.main import cavernatesouro
+ImportError: cannot import name 'cavernatesouro'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from cavernatesouro.main import cavernatesouro
+'''},
