118a0d4fb61e8133dd9f7fd478ffb6e6763ad10d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 10:56:53 -0300

    Saved trilhacaverna/main.py @2019-10-15 13:56

--- trilhacaverna/main.py
+++ trilhacaverna/main.py
@@ -5,9 +5,14 @@ from elemento.main import Elemento
 TRILHACAVERNA = "https://i.imgur.com/bNEzuBs.jpg"
 ARANHA = "https://i.imgur.com/xXPifks.png"
 COBRA = "https://i.imgur.com/EoIRfsD.png"
+SETADIREITA = "https://i.imgur.com/HiCksFh.png"
+SETAESQUERDA = "https://i.imgur.com/zpPeaMk.png"
 trilhacaverna = Cena(TRILHACAVERNA)
 trilhacaverna.vai()
 aranha = Elemento (ARANHA, cena = trilhacaverna, x= 800, y= 200, w= 200, h= 200)
 cobra = Elemento (COBRA, cena = trilhacaverna, x= 300, y= 450, w= 400, h= 200)
+setadireita = Elemento (SETADIREITA, cena = trilhacaverna, x= 800, y= 450, w= 400, h= 200)
+setaesquerda = Elemento (SETAESQUERDA, cena = trilhacaverna, x= 50, y= 550, w= 400, h= 200)
+
 
 
