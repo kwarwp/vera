65c1522abb35cb9e909823746d30f14081a5fa4b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:45:33 -0300

    Saved cavernaamigo/main.py @2019-10-15 17:45

--- cavernaamigo/main.py
+++ cavernaamigo/main.py
@@ -6,7 +6,7 @@ CAVERNAAMIGO = "https://i.imgur.com/rHNPcSh.jpg"
 AMIGO = "https://i.imgur.com/T9EbLID.png"
 GRADE = "https://i.imgur.com/B8tB6cK.png"
 CADEADO = "https://i.imgur.com/4qa6rC6.png"
-ESMERALDA = "https://i.imgur.com/Fkqy62G.jpg"
+ESMERALDA = "https://i.imgur.com/4JB2tXA.png"
 cavernaamigo = Cena(CAVERNAAMIGO)
 class Prisao:
     def __init__(self):
@@ -14,18 +14,20 @@ class Prisao:
         amigo = Elemento (AMIGO, cena = cavernaamigo, x= 600, y= 320, w= 200, h= 200)
         self.grade = Elemento (GRADE, cena = cavernaamigo, x= 510, y= 240, w= 300, h= 300)
         self.cadeado = Elemento (CADEADO, cena = cavernaamigo, drop=ativa, x= 510, y= 240, w= 50, h= 50)
+        self.esmeralda = Elemento (ESMERALDA, x= 100, y= 200, w= 100, h= 150)
         
     def abre(self, *_):
         limbo = Cena()
         self.grade.entra(limbo)
         self.cadeado.entra(limbo)
+        self.esmeralda.entra(cavernaamigo)
         
 Prisao()
 
 if __name__ == "__main__":
     cavernaamigo.vai()
     
-esmeralda = Elemento (ESMERALDA, x= 100, y= 200, w= 300, h= 400)
-esmeralda.entra(cavrnaamigo)
+
+
 
     
