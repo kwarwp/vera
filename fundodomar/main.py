4fc2d87a3d45ae7646761ef4d590d8d33492872b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 15:48:21 -0300

    Saved fundodomar/main.py @2019-10-15 18:48

--- fundodomar/main.py
+++ fundodomar/main.py
@@ -6,7 +6,10 @@ STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 FUNDODOMAR = "https://imgur.com/Mc6ktz2.png"
 ESMERALDA = "https://imgur.com/SuPb9Wa.png"
+PERGAMINHO = "https://i.imgur.com/6pz0aHn.png"
+AGUAVIVA = "https://i.imgur.com/7aIQZcW.png"
 fundodomar = Cena(FUNDODOMAR)
 fundodomar.vai()
-esmeralda = Elemento (ESMERALDA, cena = fundodomar, x= 200, y= 500, w= 50, h= 50)
-
+esmeralda = Elemento (ESMERALDA, cena = fundodomar, x= 600, y= 500, w= 50, h= 50)
+pergaminho = Elemento (PERGAMINHO, cena = fundodomar, x= 1000, y= 100, w= 100, h= 100)
+aguaviva = Elemento (AGUAVIVA, cena = fundodomar, x= 200, y= 250, w= 300, h= 200)
