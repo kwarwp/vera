2c53ac207c6257663d278607534095b5aee06f19
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:39:42 -0300

    Saved trilhacaverna/__log__.py @2019-10-15 17:39

--- trilhacaverna/__log__.py
+++ trilhacaverna/__log__.py
@@ -161,3 +161,19 @@ NameError: name 'cavrnaamigo' is not defined
     setaesquerda.vai = cavernaamigo.vai
 NameError: name 'cavernaamigo' is not defined
 '''},
+{'date': 'Tue Oct 15 2019 14:39:41.367 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    setadireita.vai = cavernatesouro.vai
+NameError: name 'cavernatesouro' is not defined
+'''},
