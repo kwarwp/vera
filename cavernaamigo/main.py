d76143bf7a1fd593e6a185f81ac7df508e386050
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 13:10:48 -0300

    Created cavernaamigo/main.py @2019-10-15 16:10

---
+++ cavernaamigo/main.py
@@ -0,0 +1,7 @@
+# vera.cavernaamigo.main.py
+from _spy.vitollino.main import Cena, STYLE
+STYLE["width"], STYLE["height"] = 1200, "650px"
+from elemento.main import Elemento
+CAVERNAAMIGO = "https://i.imgur.com/rHNPcSh.jpg"
+AMIGO = "https://i.imgur.com/T9EbLID.png"
+cavernaamigo = Cena(CAVERNAAMIGO)
