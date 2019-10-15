0396eb5f25ead334c1b8b0d6492954ee4e7571a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 12:59:36 -0300

    Saved trilhacaverna/main.py @2019-10-15 15:59

--- trilhacaverna/main.py
+++ trilhacaverna/main.py
@@ -2,6 +2,7 @@
 from _spy.vitollino.main import Cena, STYLE
 STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
+from cavernatesouro.main import cavernatesouro
 TRILHACAVERNA = "https://i.imgur.com/bNEzuBs.jpg"
 ARANHA = "https://i.imgur.com/xXPifks.png"
 COBRA = "https://i.imgur.com/EoIRfsD.png"
@@ -13,6 +14,7 @@ aranha = Elemento (ARANHA, cena = trilhacaverna, x= 800, y= 200, w= 200, h= 200)
 cobra = Elemento (COBRA, cena = trilhacaverna, x= 300, y= 450, w= 400, h= 200)
 setadireita = Elemento (SETADIREITA, cena = trilhacaverna, x= 1100, y= 450, w= 200, h= 100)
 setaesquerda = Elemento (SETAESQUERDA, cena = trilhacaverna, x= 30, y= 480, w= 100, h= 100)
+setadireita.vai = cavernatesouro.vai
 
 
 
