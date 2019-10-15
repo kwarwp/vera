fd15ddfc10973ae3216b1ad23e0081c3a8bb01d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 13:29:57 -0300

    Saved cavernatesouro/main.py @2019-10-15 16:29

--- cavernatesouro/main.py
+++ cavernatesouro/main.py
@@ -1,7 +1,10 @@
 # vera.cavernatesouro.main.py
-from _spy.vitollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, STYLE, Codigo
 STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 CAVERNATESOURO = "https://i.imgur.com/RgatDJ4.jpg"
 TESOURO = "https://i.imgur.com/FqtXDXb.jpg"
 cavernatesouro = Cena(CAVERNATESOURO)
+cavernatesouro.vai()
+codigo = Codigo ("alo", "um texto maior",cena = cavernaamigo, x= 600, y= 320, w= 200, h= 200)
+cavernatesouro.meio = codigo
