bc855bc342d57b02c3caccf845ee33a2c2c05bca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:35:29 -0300

    Saved trilhacaverna/__log__.py @2019-10-15 17:35

--- trilhacaverna/__log__.py
+++ trilhacaverna/__log__.py
@@ -127,3 +127,21 @@ Traceback (most recent call last):
     esmeralda.entra(cavrnaamigo)
 NameError: name 'cavrnaamigo' is not defined
 '''},
+{'date': 'Tue Oct 15 2019 14:35:28.551 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    from cavernaamigo.main import cavernaamigo
+  module cavernaamigo.main line 29
+    esmeralda.entra(cavrnaamigo)
+NameError: name 'cavrnaamigo' is not defined
+'''},
