09ce17fcf63a778cf6fe0b3d64df37e33db4cb9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 10:46:17 -0300

    Saved trilhacaverna/main.py @2019-10-15 13:46

--- trilhacaverna/main.py
+++ trilhacaverna/main.py
@@ -4,6 +4,10 @@ STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 TRILHACAVERNA = "https://i.imgur.com/bNEzuBs.jpg"
 ARANHA = "https://i.imgur.com/xXPifks.png"
+COBRA = "https://i.imgur.com/EoIRfsD.png"
 trilhacaverna = Cena(TRILHACAVERNA)
 trilhacaverna.vai()
 aranha = Elemento (ARANHA, cena = trilhacaverna, x= 800, y= 200, w= 200, h= 200)
+cobra = Elemento (COBRA, cena = trilhacaverna, x= 300, y= 450, w= 400, h= 200)
+
+
