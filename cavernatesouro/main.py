2cbe8c73e76fdf15a81237f9659a793d633372cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 11:07:12 -0300

    Created cavernatesouro/main.py @2019-10-15 14:07

---
+++ cavernatesouro/main.py
@@ -0,0 +1,6 @@
+# vera.cavernatesouro.main.py
+from _spy.vitollino.main import Cena, STYLE
+STYLE["width"], STYLE["height"] = 1200, "650px"
+from elemento.main import Elemento
+CAVERNATESOURO = "https://i.imgur.com/RgatDJ4.jpg"
+TESOURO = "https://i.imgur.com/FqtXDXb.jpg"
