94d41daea34bb33821135b007be43fe57e0d8204
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:12:25 -0300

    Saved cavernaamigo/main.py @2019-10-15 17:12

--- cavernaamigo/main.py
+++ cavernaamigo/main.py
@@ -4,6 +4,19 @@ STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 CAVERNAAMIGO = "https://i.imgur.com/rHNPcSh.jpg"
 AMIGO = "https://i.imgur.com/T9EbLID.png"
+GRADE = "https://i.imgur.com/B8tB6cK.png"
+CADEADO = "https://i.imgur.com/4qa6rC6.png"
 cavernaamigo = Cena(CAVERNAAMIGO)
 cavernaamigo.vai()
-amigo = Elemento (AMIGO, cena = cavernaamigo, x= 600, y= 320, w= 200, h= 200)
+class Prisao:
+    def __init__(self):
+        amigo = Elemento (AMIGO, cena = cavernaamigo, x= 600, y= 320, w= 200, h= 200)
+        self.grade = Elemento (GRADE, cena = cavernaamigo, x= 510, y= 240, w= 300, h= 300)
+        self.cadeado = Elemento (CADEADO, cena = cavernaamigo, x= 510, y= 240, w= 50, h= 50)
+        
+    def abre(self, *_):
+        limbo = Cena()
+        self.grade.entra(limbo)
+        self.cadeado.entra(limbo)
+        
+Prisao()
