024153edde5b5e070354fe7873a9b708e03d683c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 12:59:26 -0300

    Saved cavernatesouro/main.py @2019-10-15 15:59

--- cavernatesouro/main.py
+++ cavernatesouro/main.py
@@ -4,3 +4,4 @@ STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 CAVERNATESOURO = "https://i.imgur.com/RgatDJ4.jpg"
 TESOURO = "https://i.imgur.com/FqtXDXb.jpg"
+cavernatesouro = Cena(CAVERNATESOURO)
