784f971803e29989bd8a695da5c582c80856f473
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:06:16 -0300

    Saved trilhacaverna/main.py @2019-10-15 17:06

--- trilhacaverna/main.py
+++ trilhacaverna/main.py
@@ -1,5 +1,5 @@
 # vera.trilhacaverna.main.py
-from _spy.vitollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, STYLE, INVENTARIO
 STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
 from cavernatesouro.main import cavernatesouro
@@ -8,6 +8,7 @@ ARANHA = "https://i.imgur.com/xXPifks.png"
 COBRA = "https://i.imgur.com/EoIRfsD.png"
 SETADIREITA = "https://i.imgur.com/HiCksFh.png"
 SETAESQUERDA = "https://i.imgur.com/zpPeaMk.png"
+CHAVE = "https://i.imgur.com/O3oqeU0.png"
 trilhacaverna = Cena(TRILHACAVERNA)
 trilhacaverna.vai()
 aranha = Elemento (ARANHA, cena = trilhacaverna, x= 400, y= 150, w= 200, h= 200)
@@ -15,6 +16,7 @@ cobra = Elemento (COBRA, cena = trilhacaverna, x= 800, y= 450, w= 400, h= 200)
 setadireita = Elemento (SETADIREITA, cena = trilhacaverna, x= 1100, y= 450, w= 200, h= 100)
 setaesquerda = Elemento (SETAESQUERDA, cena = trilhacaverna, x= 30, y= 480, w= 100, h= 100)
 setadireita.vai = cavernatesouro.vai
-
+chave = Elemento (CHAVE, cena = trilhacaverna, x= 800, y= 450, w= 400, h= 200)
+chave.vai = lambda *_: INVENTARIO.bota(chave)
 
 
