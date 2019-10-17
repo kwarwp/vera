159329043d5d6b66b360d3d1ce0bc4f798bf2717
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 15:20:08 -0300

    Saved trilhacaverna/main.py @2019-10-17 18:20

--- trilhacaverna/main.py
+++ trilhacaverna/main.py
@@ -10,16 +10,23 @@ COBRA = "https://i.imgur.com/EoIRfsD.png"
 SETADIREITA = "https://i.imgur.com/HiCksFh.png"
 SETAESQUERDA = "https://i.imgur.com/zpPeaMk.png"
 CHAVE = "https://i.imgur.com/O3oqeU0.png"
-trilhacaverna = Cena(TRILHACAVERNA)
-INVENTARIO.inicia()
-trilhacaverna.vai()
-aranha = Elemento (ARANHA, cena = trilhacaverna, x= 400, y= 150, w= 200, h= 200)
-cobra = Elemento (COBRA, cena = trilhacaverna, x= 800, y= 450, w= 400, h= 200)
-setadireita = Elemento (SETADIREITA, cena = trilhacaverna, x= 1100, y= 450, w= 200, h= 100)
-setaesquerda = Elemento (SETAESQUERDA, cena = trilhacaverna, x= 30, y= 480, w= 100, h= 100)
-setadireita.vai = cavernatesouro.vai
-setaesquerda.vai = cavernaamigo.vai
-chave = Elemento (CHAVE, cena = trilhacaverna, tit="chave", drag = True,  x= 500, y= 450, w= 100, h= 50)
-chave.vai = lambda *_: INVENTARIO.bota(chave)
+class Caverna:
+    def __init__(self):
+        self.trilha = trilhacaverna = Cena(TRILHACAVERNA)
+        INVENTARIO.inicia()
+        #trilhacaverna.vai()
+        aranha = Elemento (ARANHA, cena = trilhacaverna, x= 400, y= 150, w= 200, h= 200)
+        cobra = Elemento (COBRA, cena = trilhacaverna, x= 800, y= 450, w= 400, h= 200)
+        setadireita = Elemento (SETADIREITA, cena = trilhacaverna, x= 1100, y= 450, w= 200, h= 100)
+        setaesquerda = Elemento (SETAESQUERDA, cena = trilhacaverna, x= 30, y= 480, w= 100, h= 100)
+        setadireita.vai = cavernatesouro.vai
+        setaesquerda.vai = cavernaamigo.vai
+        chave = Elemento (CHAVE, cena = trilhacaverna, tit="chave", drag = True,  x= 500, y= 450, w= 100, h= 50)
+        chave.vai = lambda *_: INVENTARIO.bota(chave)
+    def vai(self, *_):
+        self.trilha.vai()
 
+
+if __name__ == "__main__":
+    Caverna().vai()
 
