e59a95d9c2c9b7f0ba6701030bab8d2bad98afea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 14:46:16 -0300

    Saved fundodomar/main.py @2019-10-17 17:46

--- fundodomar/main.py
+++ fundodomar/main.py
@@ -8,16 +8,21 @@ from elemento.main import Elemento
 #from _spy.vitollino.main import Cena, STYLE, Codigo
 #STYLE["width"], STYLE["height"] = 1200, "650px"
 #from elemento.main import Elemento
+
 FUNDODOMAR = "https://imgur.com/Mc6ktz2.png"
 ESMERALDA = "https://imgur.com/SuPb9Wa.png"
+MAPA = "https://imgur.com/SuPb9Wa.png"
 PERGAMINHO = "https://i.imgur.com/6pz0aHn.png"
 AGUAVIVA = "https://i.imgur.com/7aIQZcW.png"
 PEIXE = "https://i.imgur.com/toGygPm.gif"
 fundodomar = Cena(FUNDODOMAR)
 fundodomar.vai()
+mapa = Elemento (MAPA, x= 700, y= 100, w= 150, h= 150)
 esmeralda = Elemento (ESMERALDA, cena = fundodomar, x= 600, y= 500, w= 50, h= 50)
 pergaminho = Elemento (PERGAMINHO, cena = fundodomar, x= 1000, y= 100, w= 100, h= 100)
 aguaviva = Elemento (AGUAVIVA, cena = fundodomar, x= 200, y= 250, w= 300, h= 200)
 peixe = Elemento (PEIXE, cena = fundodomar, x= 700, y= 250, w= 200, h= 100)
-pergaminho.vai = perigo1.vai
+def mostra_mapa(*_):
+    mapa.entra(fundodomar)
+pergaminho.vai = mostra_mapa
 
