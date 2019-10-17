d3af16b96758ee73670cf186de73f837d7ec6f43
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 12:02:58 -0300

    Saved perigo1/__log__.py @2019-10-17 15:02

--- perigo1/__log__.py
+++ perigo1/__log__.py
@@ -45,3 +45,34 @@ NameError: name 'tub' is not defined
     ilhamar = Perigo().ilhamar
 AttributeError: 'Perigo' object has no attribute 'ilhamar'
 '''},
+{'date': 'Thu Oct 17 2019 12:02:55.803 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    from fundodomar.main import cavernatesourofundodomar
+ImportError: cannot import name 'cavernatesourofundodomar'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    from fundodomar.main import cavernatesourofundodomar
+'''},
