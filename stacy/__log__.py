d2433bb2bb5db6b5483977ca42cb1136d82f27c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 11:00:25 -0300

    Saved stacy/__log__.py @2019-10-17 14:00

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -319,3 +319,19 @@ Exception: <TypeError: $locals_$exec_6747.Menudeanimais is not a function>
 'error': '''
 Exception: <TypeError: $locals_$exec_6796.Menudeanimais is not a function>
 '''},
+{'date': 'Thu Oct 17 2019 11:00:24.671 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 164
+    o_universo = Universo()
+NameError: name 'Universo' is not defined
+'''},
