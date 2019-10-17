a78b991db103962a162c9d6eb5df14aeaa0ea129
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 08:59:14 -0300

    Saved fundodomar/__log__.py @2019-10-17 11:59

--- fundodomar/__log__.py
+++ fundodomar/__log__.py
@@ -15,3 +15,19 @@
     fundodomar = Elemento(FUNDODOMAR, cena=fundodomar, x=700, y=398, h=900, style=dict(width= 500, height="650px"))
 NameError: name 'fundodomar' is not defined
 '''},
+{'date': 'Thu Oct 17 2019 08:59:13.423 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    codigo = Codigo ("", "",cena = fundodomar, style = dict( x= 400, y= 300))
+NameError: name 'fundodomar' is not defined
+'''},
