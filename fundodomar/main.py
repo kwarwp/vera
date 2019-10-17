1fe824ef19bdca3649d7402a784165cf5975e379
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 12:19:08 -0300

    Saved fundodomar/main.py @2019-10-17 15:19

--- fundodomar/main.py
+++ fundodomar/main.py
@@ -1,9 +1,13 @@
 # vera.fundodomar.main.py
 # vera.perigo1.main.py
 # vera.mar.main.py
-from _spy.vitollino.main import Cena, STYLE, Codigo
+from _spy.vitollino.main import Cena, STYLE, INVENTARIO
 STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
+from fundodomar.main import fundodomar
+#from _spy.vitollino.main import Cena, STYLE, Codigo
+#STYLE["width"], STYLE["height"] = 1200, "650px"
+#from elemento.main import Elemento
 FUNDODOMAR = "https://imgur.com/Mc6ktz2.png"
 ESMERALDA = "https://imgur.com/SuPb9Wa.png"
 PERGAMINHO = "https://i.imgur.com/6pz0aHn.png"
@@ -15,4 +19,5 @@ esmeralda = Elemento (ESMERALDA, cena = fundodomar, x= 600, y= 500, w= 50, h= 50
 pergaminho = Elemento (PERGAMINHO, cena = fundodomar, x= 1000, y= 100, w= 100, h= 100)
 aguaviva = Elemento (AGUAVIVA, cena = fundodomar, x= 200, y= 250, w= 300, h= 200)
 peixe = Elemento (PEIXE, cena = fundodomar, x= 700, y= 250, w= 200, h= 100)
+pergaminho.vai = perigo1.vai
 
