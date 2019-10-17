0d82889245364125cea85272425d7808b0b2cdc2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 11:33:17 -0300

    Saved stacy/main.py @2019-10-17 14:33

--- stacy/main.py
+++ stacy/main.py
@@ -185,11 +185,11 @@ class Menudeobjetos:
             
     def abre(self):        
         left, top, u = self.left, self.top, self.universo
-        self.menudeobjetos = Elemento(MENUDEOBJETOS[0], x=left -170, y=top +50, w=100, h=100, vai=self.pega,cena=self.universo)
-        self.menudeobjetos = Elemento(MENUDEOBJETOS[1], x=left +40, y=top +50, w=150, h=150, vai=self.pega,cena=self.universo)
-        self.menudeobjetos = Elemento(MENUDEOBJETOS[2], x=left -30, y=top +50, w=150, h=150, vai=self.pega,cena=self.universo)
-        self.menudeobjetos = Elemento(MENUDEOBJETOS[3], x=left -100, y=top +50, w=150, h=150, vai=self.pega,cena=self.universo)
-        self.menudeobjetos = Elemento(MENUDEOBJETOS[4], x=left -50, y=top +50, w=150, h=150, vai=self.pega,cena=self.universo)
+        self.menudeobjetos = Elemento(MENUDEOBJETOS[0], x=left -170, y=top +50, w=60, h=60, vai=self.pega,cena=self.universo)
+        self.menudeobjetos = Elemento(MENUDEOBJETOS[1], x=left +30, y=top +50, w=60, h=60, vai=self.pega,cena=self.universo)
+        self.menudeobjetos = Elemento(MENUDEOBJETOS[2], x=left -50, y=top +50, w=60, h=60, vai=self.pega,cena=self.universo)
+        self.menudeobjetos = Elemento(MENUDEOBJETOS[3], x=left -260, y=top +50, w=80, h=80, vai=self.pega,cena=self.universo)
+        self.menudeobjetos = Elemento(MENUDEOBJETOS[4], x=left -110, y=top +50, w=60, h=60, vai=self.pega,cena=self.universo)
         self.longe = Cena()
         self.na_mao = False
         # self.menudeobjetos.entra(self.universo)
