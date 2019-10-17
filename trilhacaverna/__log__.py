9e079ad50412ca987de445e25e7b50a8d1e211c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 15:29:52 -0300

    Saved trilhacaverna/__log__.py @2019-10-17 18:29

--- trilhacaverna/__log__.py
+++ trilhacaverna/__log__.py
@@ -213,3 +213,34 @@ NameError: name 'cavrnaamigo' is not defined
     esmeralda.entra(cavrnaamigo)
 NameError: name 'cavrnaamigo' is not defined
 '''},
+{'date': 'Thu Oct 17 2019 15:29:51.412 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from cavernatesouro.main import Caverna
+ImportError: cannot import name 'Caverna'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from cavernatesouro.main import Caverna
+'''},
