558f837e1e415c4f1edfd90f2606a314660d001a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 14:55:06 -0300

    Saved perigo1/__log__.py @2019-10-17 17:55

--- perigo1/__log__.py
+++ perigo1/__log__.py
@@ -108,3 +108,19 @@ TypeError: vai() missing 1 positional argument: _
     Perigo().vai()
 TypeError: vai() missing 1 positional argument: _
 '''},
+{'date': 'Thu Oct 17 2019 14:55:05.208 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    Perigo().vai()
+TypeError: vai() missing 1 positional argument: _
+'''},
