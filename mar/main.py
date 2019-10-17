d396d601cc39cd9cb72df23894a60c1fdf94b9f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 15:02:54 -0300

    Saved mar/main.py @2019-10-17 18:02

--- mar/main.py
+++ mar/main.py
@@ -2,15 +2,19 @@
 from _spy.vitollino.main import Cena, STYLE
 STYLE["width"], STYLE["height"] = 1200, "650px"
 from elemento.main import Elemento
-from perigo1.main import ilhamar as perigo
 PRAIA = "https://i.imgur.com/slKTEBc.png"
 ILHAMAR = "https://i.imgur.com/3FUDzbn.jpg"
 BARCO = "https://i.imgur.com/ckjd6cI.png"
 BOIA = "https://i.imgur.com/GmzOUTL.png"
-ilhamar = Cena(ILHAMAR, esquerda=perigo)
+ilhamar = Cena(ILHAMAR)
 praia = Elemento(PRAIA, cena=ilhamar, x=700, y=398, h=900, style=dict(width= 500, height="650px"))
 ilhamar.vai()
-barco = Elemento(BARCO, cena=ilhamar, x=70, y=430, h=500, style=dict(width= 80, height="650px"))
+def vai_fundo(*_):
+    from perigo1.main import Perigo
+    Perigo().vai()
+
+
+barco = Elemento(BARCO, cena=ilhamar, x=70, y=430, h=500, style=dict(width= 80, height="650px"), vai=vai_fundo)
 boia = Elemento(BOIA, cena=ilhamar, x=30, y=440, h=900, style=dict(width= 30, height="650px"))
 boia2 = Elemento(BOIA, cena=ilhamar, x=100, y=500, h=900, style=dict(width= 30, height="650px"))
 boia3 = Elemento(BOIA, cena=ilhamar, x=200, y=550, h=900, style=dict(width= 30, height="650px"))
