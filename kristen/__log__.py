044de74a920809e6803668458282c954b6869448
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 22:45:20 -0300

    Saved kristen/__log__.py @2019-11-18 01:45

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -850,3 +850,19 @@ TypeError: 'str' object is not callable
     baralho = (DI["FOGO"](), DI["TESOURO"](), DI["ARANHA"], DI["MUMIA"], DI["ARTEFATO1"], DI["DESABAMENTO"], DI["COBRA"])
 TypeError: 'str' object is not callable
 '''},
+{'date': 'Sun Nov 17 2019 22:45:19.642 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    baralho = (DI["FOGO"](), DI["TESOURO"](), DI["ARANHA"], DI["MUMIA"], DI["ARTEFATO1"], DI["DESABAMENTO"], DI["COBRA"])
+TypeError: 'str' object is not callable
+'''},
