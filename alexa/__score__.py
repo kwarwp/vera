3bd0e7444fa98b30d0da7bb9b32801bf7bb53689
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-02 13:28:20 -0300

    Saved alexa/__score__.py @2020-07-02 16:28

--- alexa/__score__.py
+++ alexa/__score__.py
@@ -326,3 +326,4 @@
   {'doc_id': 'cf68bea6', 'carta': 'galocha', 'casa': [700, 422], 'move': 'DROP', 'ponto': True, 'valor': 'c', '_level': 1, 'tempo': 1593707278102},
   {'doc_id': 'cf68bea6', 'carta': 'laranja', 'casa': [606, 435], 'move': 'DROP', 'ponto': True, 'valor': 'f', '_level': 1, 'tempo': 1593707292969},
   {'doc_id': 'cf68bea6', 'carta': 'maçã', 'casa': [593, 448], 'move': 'DROP', 'ponto': True, 'valor': 'f', '_level': 1, 'tempo': 1593707295117},
+  {'doc_id': 'cf68bea6', 'carta': 'coelho', 'casa': [275, 448], 'move': 'DROP', 'ponto': True, 'valor': 'b', '_level': 1, 'tempo': 1593707299879},
