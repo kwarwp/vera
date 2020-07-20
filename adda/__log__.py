11230cc9642e654ade23b060ec8a510b9d03fa0b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-20 19:31:51 -0300

    Saved adda/__log__.py @2020-07-20 22:31

--- adda/__log__.py
+++ adda/__log__.py
@@ -72,3 +72,19 @@ SyntaxError: invalid syntax
     Phaser = window.Phaser
 AttributeError: no attribute Phaser for [object Window]
 '''},
+{'date': 'Mon Jul 20 2020 19:31:46.812 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    Phaser = window.Phaser
+AttributeError: no attribute Phaser for [object Window]
+'''},
