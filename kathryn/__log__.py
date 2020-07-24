a652340152489606fba79b811d78b0dd348df4ac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 12:19:33 -0300

    Saved kathryn/__log__.py @2020-07-24 15:19

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -344,3 +344,42 @@ Traceback (most recent call last):
   module kathryn.main line 5
     from natalia.main import Jogador
 '''},
+{'date': 'Fri Jul 24 2020 12:19:34.377 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from natalia.main import Jogador
+  module natalia.main line 2
+    from kathryn.main import Rodada
+  module kathryn.main line 5
+    from natalia.main import Jogador
+ImportError: cannot import name 'Jogador'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    from natalia.main import Jogador
+  module natalia.main line 2
+    from kathryn.main import Rodada
+  module kathryn.main line 5
+    from natalia.main import Jogador
+'''},
