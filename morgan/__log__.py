298a6c7f2992647c20f91a79e91ef5ea094ffee0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 10:23:37 -0300

    Saved morgan/__log__.py @2022-07-14 13:23

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -219,3 +219,27 @@ NameError: name 'LEAO_USA' is not defined
     self.mordida = Elem(MORDIDA, x=0, y=0, w=1400, h=650, style={"opacity": 0.5}, tipo="100% 100%") #, vai=self.antidoto)
 NameError: name 'Elem' is not defined
 '''},
+{'date': 'Thu Jul 14 2022 10:23:37.396 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 95
+    a_floresta = FlorestaLeao()
+  module <module> line 85
+    leao = Leao(self.floresta_inicio)
+  module <module> line 53
+    self.mordida = Elem(MORDIDA, x=0, y=0, w=1400, h=650, style={"opacity": 0.5}, tipo="100% 100%") #, vai=self.antidoto)
+  module elemento.main line 18
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
