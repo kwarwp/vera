96f965d56a3f2e4083f1016edd147cc828a381bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 10:27:57 -0300

    Saved morgan/__log__.py @2022-07-14 13:27

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -243,3 +243,23 @@ NameError: name 'Elem' is not defined
     self.elt.style.backgroundImage = "url({})".format(value)
 AttributeError: 'Elemento' object has no attribute 'elt'
 '''},
+{'date': 'Thu Jul 14 2022 10:27:56.619 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 95
+    a_floresta = FlorestaLeao()
+  module <module> line 85
+    leao = Leao(self.floresta_inicio)
+  module <module> line 53
+    self.mordida = Elem(MORDIDA, x=0, y=0, w=1400, h=650, style={"opacity": 0.5}, tipo="100% 100%") #, vai=self.antidoto)
+NameError: name 'Elem' is not defined
+'''},
