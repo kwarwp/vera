b629f7b59629a5295c06dcb3e945b119e03b9afc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 10:48:41 -0300

    Saved adda/__log__.py @2022-07-14 13:48

--- adda/__log__.py
+++ adda/__log__.py
@@ -88,3 +88,19 @@ AttributeError: no attribute Phaser for [object Window]
     Phaser = window.Phaser
 AttributeError: no attribute Phaser for [object Window]
 '''},
+{'date': 'Thu Jul 14 2022 10:48:40.929 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    Phaser = window.Phaser
+AttributeError: no attribute Phaser for [object Window]
+'''},
