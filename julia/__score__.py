fbd5eba3647d33fb8b2fac2ee562f4c4704a39d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 10:42:35 -0300

    Saved julia/__score__.py @2022-07-14 13:42

--- julia/__score__.py
+++ julia/__score__.py
@@ -80,3 +80,4 @@
 {'doc_id': 'adfcb32e', 'carta': '', 'casa': 'pescaria', 'move': 'JULIA', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1571661391589},
 {'doc_id': '781341dd', 'carta': '', 'casa': 'pescaria', 'move': 'JULIA', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1571661692114},
 {'doc_id': '6eca6c82', 'carta': 'oli', 'casa': 'pescaria', 'move': 'JULIA', 'ponto': 'OK', 'valor': '', '_level': 0, 'tempo': 1657806141345},
+  {'doc_id': '6eca6c82', 'carta': 'julia', 'casa': [893, 393], 'move': 'CRIA', 'ponto': True, 'valor': 0, '_level': 1, 'tempo': 1657806155011},
