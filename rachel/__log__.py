97aa57247bb9ceaa1077879666ae09459f85acc2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 10:24:57 -0300

    Saved rachel/__log__.py @2022-07-14 13:24

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -273,3 +273,27 @@ SyntaxError: invalid syntax
                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jul 14 2022 10:24:56.856 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 100
+    a_floresta = FlorestaBanana()
+  module <module> line 92
+    banana = Banana(self.floresta_inicio)
+  module <module> line 44
+    self.banana = Elemento(BANANA, x=230, y=500, style=dict(width="50px"), vai=self.pega)
+  module elemento.main line 18
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
