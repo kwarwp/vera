7bae74a5027ff8c06e6239987329dc793cabde33
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 15:14:05 -0300

    Saved samantha/__log__.py @2024-10-01 18:14

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -567,3 +567,25 @@ AttributeError: 'JogoLetra' object has no attribute 'letra_h'
                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 01 2024 15:14:05.382 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 119
+    JogoLetra()
+  module <module> line 56
+    self.letra_a = Elemento(LETRA_A,tit="A", x=540, y=470, w=90, h=20, drag=True,vai=self.clicounaletra)
+  module elemento.main line 18
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
